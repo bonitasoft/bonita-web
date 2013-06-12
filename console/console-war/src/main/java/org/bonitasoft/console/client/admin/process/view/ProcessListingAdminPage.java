f274f2b17f23e3ac676c61885ab6039c130576de
Fabio Lombardi <fabio.lombardi@bonitasoft.com>
2013-06-12 16:52:12 +0000

    WEB-2259: delete button for disabled apps (middle column) has no confirmation window

--- console/console-war/src/main/java/org/bonitasoft/console/client/admin/process/view/ProcessListingAdminPage.java
+++ console/console-war/src/main/java/org/bonitasoft/console/client/admin/process/view/ProcessListingAdminPage.java
@@ -162,8 +162,7 @@ public class ProcessListingAdminPage extends ItemListingPage<ProcessItem>
                 createItemTable()
                         .addGroupedAction(
                                 new JsId("enable"), _("Enable"), _("Enable selected apps"), new EnableProcessAction())
-                        .addGroupedAction(
-                                new JsId("delete"), _("Delete"), _("Delete selected apps"), new DeleteProcessesAction()),
+                        .addGroupedMultipleDeleteAction(_("Delete selected apps"), ProcessDefinition.get(), _("app"), _("apps")),
                 new ProcessQuickDetailsAdminPage());
     }
 
