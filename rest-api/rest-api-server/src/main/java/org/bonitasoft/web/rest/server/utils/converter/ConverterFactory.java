0e719fe595282a010986fc61180a3a84dd40fba2
Colin Puy <colin.puy@bonitasoft.com>
2013-06-17 12:54:24 +0000

    [REST API] change package declaration of rest server

---
+++ rest-api/rest-api-server/src/main/java/org/bonitasoft/web/rest/server/utils/converter/ConverterFactory.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2012 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.web.rest.server.utils.converter;
+
+import org.bonitasoft.web.rest.server.utils.converter.typed.BooleanConverter;
+import org.bonitasoft.web.rest.server.utils.converter.typed.DoubleConverter;
+import org.bonitasoft.web.rest.server.utils.converter.typed.IntegerConverter;
+import org.bonitasoft.web.rest.server.utils.converter.typed.LongConverter;
+import org.bonitasoft.web.rest.server.utils.converter.typed.StringConverter;
+
+/**
+ * @author Colin PUY
+ * 
+ */
+public class ConverterFactory {
+
+    public Converter<?> createConverter(String className) {
+        if (isClass(String.class, className)) {
+            return new StringConverter();
+        // FIXME uncomment when ENGINE-1099 resolved and DateConverter is implemented
+//        } else if (isClass(Date.class, className)) {
+//            return new DateConverter();
+        } else if (isClass(Double.class, className)) {
+            return new DoubleConverter();
+        } else if (isClass(Long.class, className)) {
+            return new LongConverter();
+        }  else if (isClass(Boolean.class, className)) {
+            return new BooleanConverter();
+        } else if (isClass(Integer.class, className)) {
+            return new IntegerConverter();
+        }
+        throw new UnsupportedOperationException("Canno't create converter for class name : " + className);
+    }
+
+    private boolean isClass(Class<?> classe, String className) {
+        return classe.getName().equals(className);
+    }
+}
