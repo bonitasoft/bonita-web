62e66c4ea1e1b818cc80eca8f8f34271704a44e1
Colin Puy <colin.puy@bonitasoft.com>
2013-06-26 07:49:54 +0000

    [CLEAN] move documentUploadServlet

--- common/common-server/src/main/java/org/bonitasoft/console/common/server/servlet/DocumentUploadServlet.java
+++ common/common-server/src/main/java/org/bonitasoft/console/common/server/servlet/DocumentUploadServlet.java
@@ -12,7 +12,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.bonitasoft.console.common.server.document.servlet;
+package org.bonitasoft.console.common.server.servlet;
 
 import java.io.File;
 import java.io.IOException;
