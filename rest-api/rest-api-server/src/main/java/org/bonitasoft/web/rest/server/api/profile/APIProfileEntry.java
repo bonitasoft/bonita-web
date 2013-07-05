e214448534bb2906a4da9b0f11448379e4e1d158
Paul Amar <paul.amar@bonitasoft.com>
2013-07-05 14:40:09 +0000

    Remove add, update & delete in the APIProfileEntry. (included in the APIProfileEntryExt)

--- rest-api/rest-api-server/src/main/java/org/bonitasoft/web/rest/server/api/profile/APIProfileEntry.java
+++ rest-api/rest-api-server/src/main/java/org/bonitasoft/web/rest/server/api/profile/APIProfileEntry.java
@@ -16,14 +16,9 @@
  */
 package org.bonitasoft.web.rest.server.api.profile;
 
-import static org.bonitasoft.web.rest.model.portal.profile.ProfileEntryItem.ATTRIBUTE_CUSTOM_NAME;
 import static org.bonitasoft.web.rest.model.portal.profile.ProfileEntryItem.ATTRIBUTE_INDEX;
-import static org.bonitasoft.web.rest.model.portal.profile.ProfileEntryItem.ATTRIBUTE_NAME;
-import static org.bonitasoft.web.rest.model.portal.profile.ProfileEntryItem.ATTRIBUTE_PAGE;
-import static org.bonitasoft.web.rest.model.portal.profile.ProfileEntryItem.ATTRIBUTE_TYPE;
 
 import java.util.List;
-import java.util.Map;
 
 import org.bonitasoft.web.rest.model.portal.profile.ProfileEntryDefinition;
 import org.bonitasoft.web.rest.model.portal.profile.ProfileEntryItem;
@@ -35,26 +30,17 @@ import org.bonitasoft.web.rest.server.datastore.profile.entry.SearchProfileEntri
 import org.bonitasoft.web.rest.server.engineclient.EngineAPIAccessor;
 import org.bonitasoft.web.rest.server.engineclient.EngineClientFactory;
 import org.bonitasoft.web.rest.server.engineclient.ProfileEntryEngineClient;
-import org.bonitasoft.web.rest.server.framework.api.APIHasAdd;
-import org.bonitasoft.web.rest.server.framework.api.APIHasDelete;
 import org.bonitasoft.web.rest.server.framework.api.APIHasGet;
 import org.bonitasoft.web.rest.server.framework.api.APIHasSearch;
-import org.bonitasoft.web.rest.server.framework.api.APIHasUpdate;
 import org.bonitasoft.web.rest.server.framework.api.Datastore;
-import org.bonitasoft.web.toolkit.client.common.exception.api.APIAttributeMissingException;
-import org.bonitasoft.web.toolkit.client.common.util.MapUtil;
-import org.bonitasoft.web.toolkit.client.data.APIID;
 
 /**
  * @author Nicolas Tith
  * @author Séverin Moussel
  */
 public class APIProfileEntry extends ConsoleAPI<ProfileEntryItem> implements
-        APIHasAdd<ProfileEntryItem>,
-        APIHasUpdate<ProfileEntryItem>,
         APIHasGet<ProfileEntryItem>,
-        APIHasSearch<ProfileEntryItem>,
-        APIHasDelete
+        APIHasSearch<ProfileEntryItem>        
 {
 
     @Override
@@ -62,39 +48,7 @@ public class APIProfileEntry extends ConsoleAPI<ProfileEntryItem> implements
         return ProfileEntryDefinition.get();
     }
 
-    @Override
-    public ProfileEntryItem add(final ProfileEntryItem item) {
-
-        checkDatas(item.getAttributes());
-
-        return super.add(item);
-    }
-
-    @Override
-    public ProfileEntryItem update(final APIID id, final Map<String, String> attributes) {
-
-        checkDatas(attributes);
-
-        return super.update(id, attributes);
-    }
-
-    /**
-     * Check that at least one name is passed (Default or Custom)
-     * 
-     * @param attributes
-     *            The attributes passed to the API
-     */
-    protected void checkDatas(final Map<String, String> attributes) {
-        if (MapUtil.isBlank(attributes, ATTRIBUTE_NAME) && MapUtil.isBlank(attributes, ATTRIBUTE_CUSTOM_NAME)) {
-            throw new APIAttributeMissingException(ATTRIBUTE_NAME);
-        }
-
-        if (ProfileEntryItem.VALUE_TYPE.link.equals(attributes.get(ATTRIBUTE_TYPE)) && MapUtil.isBlank(attributes, ATTRIBUTE_PAGE)) {
-            throw new APIAttributeMissingException(ATTRIBUTE_PAGE);
-        }
-
-    }
-
+   
     @Override
     public String defineDefaultSearchOrder() {
         return ATTRIBUTE_INDEX;
