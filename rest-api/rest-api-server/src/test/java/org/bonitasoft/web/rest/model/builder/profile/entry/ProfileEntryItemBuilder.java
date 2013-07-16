9ae0a882a1a5eb8ac88011924e8bf00d3d30cb85
Rohart bastien <rohartbastien@bonitasoft.com>
2013-07-16 17:59:50 +0200

    repare profiles

--- rest-api/rest-api-server/src/test/java/org/bonitasoft/web/rest/model/builder/profile/entry/ProfileEntryItemBuilder.java
+++ rest-api/rest-api-server/src/test/java/org/bonitasoft/web/rest/model/builder/profile/entry/ProfileEntryItemBuilder.java
@@ -1,69 +1,3 @@
-<<<<<<< HEAD
-/**
- * Copyright (C) 2012 BonitaSoft S.A.
- * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2.0 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-package org.bonitasoft.web.rest.model.builder.profile.entry;
-
-import org.bonitasoft.engine.profile.ProfileEntry;
-import org.bonitasoft.web.rest.model.portal.profile.ProfileEntryItem;
-import org.bonitasoft.web.rest.model.portal.profile.ProfileEntryItem.VALUE_TYPE;
-
-/**
- * @author Vincent Elcrin
- * 
- */
-public class ProfileEntryItemBuilder extends AbstractProfileEntryBuilder<ProfileEntryItem> {
-
-    protected ProfileEntryItemBuilder() {
-        // empty
-    }
-
-    public static ProfileEntryItemBuilder aProfileEntryItem() {
-        return new ProfileEntryItemBuilder();
-    }
-
-    public ProfileEntryItem build() {
-        ProfileEntryItem item = new ProfileEntryItem();
-        item.setId(id);
-        item.setName(name);
-        // item.setCustomName(custormName);
-        item.setProfileId(profileId);
-        item.setIndex(index);
-        item.setType(type);
-        item.setDescription(description);
-        item.setParentId(parentId);
-        item.setPage(page);
-        return item;
-    }
-
-    public ProfileEntryItemBuilder from(ProfileEntry profileEntry) {
-        id = profileEntry.getId();
-        name = profileEntry.getName();
-        // custormName = profileEntry.getName();
-        profileId = profileEntry.getProfileId();
-        index = profileEntry.getIndex();
-        type = VALUE_TYPE.valueOf(profileEntry.getType());
-        description = profileEntry.getDescription();
-        parentId = profileEntry.getParentId();
-        page = profileEntry.getPage();
-        return this;
-    }
-
-}
-=======
 /**
  * Copyright (C) 2012 BonitaSoft S.A.
  * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
@@ -117,7 +51,7 @@ public class ProfileEntryItemBuilder extends AbstractProfileEntryBuilder<Profile
     public ProfileEntryItemBuilder from(ProfileEntry profileEntry) {
         id = profileEntry.getId();
         name = profileEntry.getName();
-//        custormName = profileEntry.getName();
+        // custormName = profileEntry.getName();
         profileId = profileEntry.getProfileId();
         index = profileEntry.getIndex();
         type = VALUE_TYPE.valueOf(profileEntry.getType());
@@ -128,4 +62,3 @@ public class ProfileEntryItemBuilder extends AbstractProfileEntryBuilder<Profile
     }
 
 }
->>>>>>> ffcd84e677e1e3469e37b7d4890fc392a9908f43
