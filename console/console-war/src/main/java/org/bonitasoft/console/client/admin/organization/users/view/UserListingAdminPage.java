71e3eae3f2807cfd76a442da38b0b5edfd98019e
cpuy <colin.puy@bonitasoft.com>
2013-07-26 15:03:11 +0200

    delete abstractUserListing

--- console/console-war/src/main/java/org/bonitasoft/console/client/admin/organization/users/view/UserListingAdminPage.java
+++ console/console-war/src/main/java/org/bonitasoft/console/client/admin/organization/users/view/UserListingAdminPage.java
@@ -19,20 +19,42 @@ package org.bonitasoft.console.client.admin.organization.users.view;
 import static java.util.Arrays.asList;
 import static org.bonitasoft.web.toolkit.client.common.i18n.AbstractI18n._;
 
+import java.util.LinkedList;
 import java.util.List;
 
+import org.bonitasoft.web.rest.model.identity.UserDefinition;
+import org.bonitasoft.web.rest.model.identity.UserItem;
+import org.bonitasoft.web.toolkit.client.data.item.Definitions;
+import org.bonitasoft.web.toolkit.client.data.item.attribute.reader.DateAttributeReader;
+import org.bonitasoft.web.toolkit.client.ui.JsId;
 import org.bonitasoft.web.toolkit.client.ui.action.ActionShowPopup;
 import org.bonitasoft.web.toolkit.client.ui.action.CheckValidSessionBeforeAction;
 import org.bonitasoft.web.toolkit.client.ui.component.Clickable;
 import org.bonitasoft.web.toolkit.client.ui.component.Link;
+import org.bonitasoft.web.toolkit.client.ui.component.table.ItemTable;
+import org.bonitasoft.web.toolkit.client.ui.page.itemListingPage.ItemListingFilter;
+import org.bonitasoft.web.toolkit.client.ui.page.itemListingPage.ItemListingPage;
+import org.bonitasoft.web.toolkit.client.ui.page.itemListingPage.ItemListingResourceFilter;
+import org.bonitasoft.web.toolkit.client.ui.page.itemListingPage.ItemListingSort;
+import org.bonitasoft.web.toolkit.client.ui.page.itemListingPage.ItemListingTable;
+import org.bonitasoft.web.toolkit.client.ui.utils.DateFormat.FORMAT;
 
 /**
  * @author Paul AMAR
  * 
  */
-public class UserListingAdminPage extends AbstractUserListingPage {
-
+public class UserListingAdminPage extends ItemListingPage<UserItem> {
+    
     public static final String TOKEN = "userlistingadmin";
+
+    private static final String TABLE_ALL_USERS = "allusers";
+    
+    private static final String FILTER_PRIMARY_ALL_USERS = "allusersfilter";
+
+    @Override
+    public void defineTitle() {
+        this.setTitle(_("Users"));
+    }
 
     @Override
     protected List<Clickable> defineFilterPanelActions() {
@@ -40,12 +62,49 @@ public class UserListingAdminPage extends AbstractUserListingPage {
     }
 
     private Clickable addUserLink() {
-        return new Link(_("Create a user"), _("Opens a popup to create a user"), new CheckValidSessionBeforeAction(new ActionShowPopup(new PopupAddUserPage())));
+        return new Link(_("Create a user"), _("Opens a popup to create a user"), 
+                new CheckValidSessionBeforeAction(new ActionShowPopup(new PopupAddUserPage())));
     }
 
     @Override
+    protected LinkedList<ItemListingFilter> definePrimaryFilters() {
+        final LinkedList<ItemListingFilter> filters = new LinkedList<ItemListingFilter>();
+        filters.add(new ItemListingFilter(FILTER_PRIMARY_ALL_USERS, _("All"), _("All Users"), TABLE_ALL_USERS));
+        return filters;
+    }
+
+    @Override
+    protected ItemListingSort defineDefaultSort() {
+        return new ItemListingSort(UserItem.ATTRIBUTE_USERNAME, true);
+    }
+
+    @Override
+    protected LinkedList<ItemListingTable> defineTables() {
+        final LinkedList<ItemListingTable> tables = new LinkedList<ItemListingTable>();
+        tables.add(allUserTable());
+        return tables;
+    }
+
+    protected ItemListingTable allUserTable() {
+        return new ItemListingTable(new JsId(TABLE_ALL_USERS), _("Enabled"), itemTable(), getItemQuickDetailPage());
+    }
+
+    protected ItemTable itemTable() {
+        return new ItemTable(Definitions.get(UserDefinition.TOKEN))
+                .addColumn(UserItem.ATTRIBUTE_ICON, _("Icon"))
+                .addColumn(UserItem.ATTRIBUTE_FIRSTNAME, _("First name"), true)
+                .addColumn(UserItem.ATTRIBUTE_LASTNAME, _("Last name"), true)
+                .addColumn(new DateAttributeReader(UserItem.ATTRIBUTE_LAST_CONNECTION_DATE, FORMAT.DISPLAY_RELATIVE), _("Last login date"))
+                .addColumn(UserItem.ATTRIBUTE_JOB_TITLE, _("Job title"), false);
+    }
+
     protected UserQuickDetailsPage getItemQuickDetailPage() {
         return new UserQuickDetailsAdminPage();
+    }
+
+    @Override
+    protected ItemListingResourceFilter defineResourceFilters() {
+        return null;
     }
 
     @Override
