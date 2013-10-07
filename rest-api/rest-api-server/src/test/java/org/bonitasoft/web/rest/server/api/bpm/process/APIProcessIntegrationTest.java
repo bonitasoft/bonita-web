fa877552c9713d1b4f463d107dd72f4a1eaf9c7a
ntith <nicolas.tith@bonitasoft.com>
2013-10-07 17:22:41 +0200

    WEB-3033 As an application developper, I need a REST API to Get lastest process definition

--- rest-api/rest-api-server/src/test/java/org/bonitasoft/web/rest/server/api/bpm/process/APIProcessIntegrationTest.java
+++ rest-api/rest-api-server/src/test/java/org/bonitasoft/web/rest/server/api/bpm/process/APIProcessIntegrationTest.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.bonitasoft.engine.api.ProcessAPI;
