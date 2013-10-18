98cc739c39ac85ea8b8a4c7f03d324210ccf9b73
Colin PUY <colin.puy@bonitasoft.com>
2013-10-18 15:54:51 +0200

    [REFACTOR] refactor SelectUserAndAssignTaskPage

--- console/console-war/src/main/java/org/bonitasoft/console/client/admin/bpm/task/view/SelectUserAndAssignTaskPage.java
+++ console/console-war/src/main/java/org/bonitasoft/console/client/admin/bpm/task/view/SelectUserAndAssignTaskPage.java
@@ -16,10 +16,10 @@
  */
 package org.bonitasoft.console.client.admin.bpm.task.view;
 
+import static java.util.Arrays.asList;
 import static org.bonitasoft.web.toolkit.client.common.i18n.AbstractI18n._;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.bonitasoft.console.client.admin.bpm.cases.view.CaseListingAdminPage;
@@ -31,7 +31,6 @@ import org.bonitasoft.console.client.data.item.attribute.reader.UserAttributeRea
 import org.bonitasoft.web.rest.model.identity.UserDefinition;
 import org.bonitasoft.web.rest.model.identity.UserItem;
 import org.bonitasoft.web.toolkit.client.ui.Page;
-import org.bonitasoft.web.toolkit.client.ui.action.Action;
 import org.bonitasoft.web.toolkit.client.ui.action.form.FormAction;
 
 /**
@@ -51,77 +50,49 @@ public class SelectUserAndAssignTaskPage extends Page {
         PRIVILEGES.add("reportlistingadminext");
     }
 
-    @Override
-    public String defineToken() {
-        return TOKEN;
+    public void defineTitle() {
+        setTitle(_("Select user to assign task"));
+    
     }
 
-    protected Action defineSubmitButtonAction() {
-        // need to use a form action otherwise hidden field are not passed though to the action
-        return new FormAction() {
+    @Override
+    public final void buildView() {
+        SelectItemAndDoForm selectItemAndDoForm = new SelectItemAndDoForm(
+                asList(selectUserAndDoEntry()),
+                _("Assign"),
+                _("Assign selected tasks to user"),
+                assignTaskAction());
+        selectItemAndDoForm.setHiddenEntries(getParameters());
+    
+        addBody(selectItemAndDoForm);
+    }
 
+    private SelectItemAndDoEntry selectUserAndDoEntry() {
+        return new SelectItemAndDoEntry(
+                "user_id",
+                _("Select a user"),
+                _("Select a user by typing a part of his name."),
+                UserDefinition.get(),
+                new UserAttributeReader(),
+                UserItem.ATTRIBUTE_ID);
+    }
+
+    private FormAction assignTaskAction() {
+        return new FormAction() {
+        
             @Override
             public void execute() {
                 final AssignTaskAndHistoryBackAction action = new AssignTaskAndHistoryBackAction();
                 action.setParameters(getParameters());
                 action.execute();
             }
-
+        
         };
     }
 
-    protected String defineSubmitButtonLabel() {
-        return _("Assign");
-    }
-
-    protected String defineSubmitButtonTooltip() {
-        return _("Assign selected tasks to user");
-    }
-
-    public void defineTitle() {
-        setTitle(_("Select user to assign task"));
-
-    }
-
-    /**
-     * Override this method to change the name of the parameter passed to the callback action
-     * 
-     * @return This method must return the name of the parameter
-     */
-    protected String defineUserIdParameterName() {
-        return "user_id";
-    }
-
-    protected final List<SelectItemAndDoEntry> defineEntries() {
-        return Arrays.asList(
-                new SelectItemAndDoEntry(
-                        defineUserIdParameterName(),
-                        _("Select a user"),
-                        _("Select a user by typing a part of his name."),
-                        UserDefinition.get(),
-                        new UserAttributeReader(),
-                        UserItem.ATTRIBUTE_ID)
-                );
-    }
-
     @Override
-    public final void buildView() {
-        // Before
-        buildBefore();
-    
-        // Build form
-        final SelectItemAndDoForm selectItemAndDoForm = new SelectItemAndDoForm(
-                defineEntries(),
-                defineSubmitButtonLabel(),
-                defineSubmitButtonTooltip(),
-                defineSubmitButtonAction());
-        selectItemAndDoForm.setHiddenEntries(getParameters());
-    
-        addBody(selectItemAndDoForm);
-    }
-
-    protected void buildBefore() {
-        // Do nothing if not overriden
+    public String defineToken() {
+        return TOKEN;
     }
 
 }
