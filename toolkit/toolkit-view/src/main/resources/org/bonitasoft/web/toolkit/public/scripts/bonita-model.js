a312c03c2460ddafc5443e9392ce2d6d9afaba24
Vincent Elcrin <vincent.elcrin@bonitasoft.com>
2013-11-18 10:36:15 +0100

    Changes for a more flexible model.

--- toolkit/toolkit-view/src/main/resources/org/bonitasoft/web/toolkit/public/scripts/bonita-model.js
+++ toolkit/toolkit-view/src/main/resources/org/bonitasoft/web/toolkit/public/scripts/bonita-model.js
@@ -1,10 +1,10 @@
 /* global bonitasoft:false */
 
-(function (bonitasoft, namespace, http) {
+(function (bonitasoft, assertion, namespace, http) {
     "use strict";
-    
-    function Search(url) {
-        return function(parameters, callback) {
+
+    var createSearch = function (url) {
+        return function (parameters, callback) {
             var target = url;
             target += parameters.page ? '?p=' + parameters.page : '?p=0';
             target += parameters.count ? '&c=' + parameters.count : '&c=100';
@@ -12,35 +12,79 @@
             target += parameters.filter ? '&f=' + parameters.filter : '';
             http.get(target, callback);
         };
-    }
+    };
 
-    function Fetch(url) {
-        return function(id, callback) {
+    var createGet = function (url) {
+        return function (id, callback) {
             http.get(url + '/' + id, callback);
         };
-    }
+    };
 
+    var api = {
+        create: function (configuration) {
+            assertion.isDefined(configuration.url);
+
+            var url = configuration.url,
+                api = {};
+
+            if (configuration.hasGet === true) {
+                api.get = configuration.get || createGet(url);
+            }
+
+            if (configuration.hasSearch === true) {
+                api.search = configuration.search || createSearch(url);
+            }
+
+            return api;
+        },
+        callback: function (callback) {
+            return function(code, response) {
+                if(code == 200) {
+                    callback.onSuccess && callback.onSuccess(JSON.parse(response));
+                } else {
+                    callback.onFail && callback.onFail(code, response);
+                }
+            }
+        }
+
+    };
+    
     namespace.extend(bonitasoft, 'model', function (model) {
+        model.callback = api.callback;
         /*
-         * Namespace allowing access to processes REST API 
+         * Namespace allowing access to processes REST API
          */
         model.processes = model.processes || (function () {
-            var url = '../API/bpm/process';
-            return {
-                fetch: new Fetch(url),
-                search: new Search(url)
-            };
+            return api.create({
+                url: '../API/bpm/process',
+
+                hasGet: true,
+                hasSearch: true
+            });
+        })();
+
+        /*
+         * Namespace allowing access to cases REST API
+         */
+        model.cases = model.cases || (function () {
+            return api.create({
+                url: '../API/bpm/case',
+
+                hasGet: true,
+                hasSearch: true
+            });
         })();
         
         /*
-         * Namespace allowing access to cases REST API 
+         * Namespace allowing access to archived cases REST API
          */
-        model.cases = model.cases || (function () {
-            var url = '../API/bpm/case';
-            return {
-                fetch: new Fetch(url),
-                search: new Search(url)
-            };
+        model.cases.archives = model.cases.archives || (function () {
+            return api.create({
+                url: '../API/bpm/archivedCase',
+
+                hasGet: true,
+                hasSearch: true
+            });
         })();
     });
-}) (bonitasoft, bonitasoft.utils.namespace, bonitasoft.utils.http);
+})(bonitasoft, bonitasoft.utils.assertion, bonitasoft.utils.namespace, bonitasoft.utils.http);
