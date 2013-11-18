6bbfba2b83226bf039a055bdd70c2cd60802f7e8
Vincent Elcrin <vincent.elcrin@bonitasoft.com>
2013-11-18 10:37:56 +0100

    Add tests and javascript poc.

--- toolkit/toolkit-view/src/test/resources/jasmine/test/bonita-utils-test.js
+++ toolkit/toolkit-view/src/test/resources/jasmine/test/bonita-utils-test.js
@@ -1,23 +1,32 @@
-describe("Bonita utils", function() {
-    
+/* global   bonitasoft:false,
+
+            jasmine:false,
+            beforeEach,
+            describe:false,
+            it:false,
+            expect:false */
+
+describe("Bonita utils", function () {
+
     var utils = bonitasoft.utils;
-    
+
     /*
      * Namespacing
      */
-    describe("should be able to extend a namespace", function() {
-        
-        it('from a non existing path', function() {
-            var extension = 'extension', root = {};
-            
-            utils.namespace.extend(root, 'path.which.doesnt.exist', function(exist) {
+    describe("should be able to extend a namespace", function () {
+
+        it('from a non existing path', function () {
+            var extension = 'extension',
+                root = {};
+
+            utils.namespace.extend(root, 'path.which.doesnt.exist', function (exist) {
                 exist.extension = extension;
             });
-            
+
             expect(root.path.which.doesnt.exist.extension).toEqual(extension);
         });
-        
-        it("from an existing path", function() {
+
+        it("from an existing path", function () {
             var extension = 'extension',
                 existing = 'existing',
                 root = {
@@ -27,72 +36,127 @@ describe("Bonita utils", function() {
                         }
                     }
                 };
-            
-                utils.namespace.extend(root, 'to.extend', function(extend) {
-                    extend.extension = extension;
-                });
-            
-                expect(root.to.extend.existing).toEqual(existing);
-                expect(root.to.extend.extension).toEqual(extension);
+
+            utils.namespace.extend(root, 'to.extend', function (extend) {
+                extend.extension = extension;
+            });
+
+            expect(root.to.extend.existing).toEqual(existing);
+            expect(root.to.extend.extension).toEqual(extension);
         });
     });
-    
+
     /*
      * Assertion
      */
-    describe("should provide assertion", function() {
-        
-        it("which can success", function() {
+    describe("should provide assertion", function () {
+
+        it("which can success", function () {
             var foo = '';
-            
+
             utils.assertion.assert(foo !== undefined);
         });
-        
-        it("which can fail", function() {
+
+        it("which can fail", function () {
             var foo,
                 message = 'foo must be initialized';
-            
-            expect(function() {
-                utils.assertion.assert(foo !== undefined, message)
+
+            expect(function () {
+                utils.assertion.assert(foo !== undefined, message);
             }).toThrow(message);
         });
     });
-    
+
     /*
      * Variable
      */
-    describe("should provide variable replacement in html context", function() {
-        
-        it("which assert if context is undefined", function() {
+    describe("should provide variable replacement in html context", function () {
+
+        it("which assert if context is undefined", function () {
             var context;
-            
-            expect(function() {
+
+            expect(function () {
                 utils.variable.inject(context, "name", "value");
             }).toThrow(jasmine.any(String));
-            
+
         });
-        
-        it("which replace a variable if context contains it", function() {
-            var context = jasmine.createSpyObj('html', [ 'html' ]);
-            context.html.andCallFake(function() {
+
+        it("which replace a variable if context contains it", function () {
+            var context = jasmine.createSpyObj('html', ['html']);
+            context.html.andCallFake(function () {
                 return 'variable = %variable%';
             });
-            
+
             utils.html.variable.inject(context, 'variable', 'value');
-            
+
             expect(context.html).toHaveBeenCalledWith('variable = value');
         });
-        
-        it("which replace multiple variables if context contains them", function() {
-            var context = jasmine.createSpyObj('html', [ 'html' ]);
-            context.html.andCallFake(function() {
+
+        it("which replace multiple variables if context contains them", function () {
+            var context = jasmine.createSpyObj('html', ['html']);
+            context.html.andCallFake(function () {
                 return 'variable 1 = %variable%, variable 2 = %variable%';
             });
-            
+
             utils.html.variable.inject(context, 'variable', 'value');
-            
+
             expect(context.html).toHaveBeenCalledWith('variable 1 = value, variable 2 = value');
         });
     });
-    
+
+    /*
+     * Form utils
+     */
+    describe("provide form utilities", function () {
+
+        var renderer = function (item) {
+            return {
+                text: item.text,
+                value: item.value
+            };
+        },
+            element, select;
+
+        beforeEach(function () {
+            element = jasmine.createSpyObj('select', ['html', 'append', 'data']);
+            select = utils.form.select.wrap(element);
+        });
+
+        it("that should provide a method to clear a select", function () {
+
+            select.clear();
+
+            expect(element.html).toHaveBeenCalledWith('');
+        });
+
+        it("that should be able to add options of a select", function () {
+
+            var options = [createOption(1), createOption(2)];
+
+            select.add(options, renderer);
+
+            verifyAppendSequence(element, options);
+
+        });
+
+        function createOption(value) {
+            var label = 'label' + value;
+            return {
+                text: label,
+                value: value,
+                outerHTML: "<option value=\"" + value + "\">" + label + "</option>"
+            };
+        }
+
+        /*
+         * Verify options that have been append to the element's html.
+         * The order matter.
+         */
+        function verifyAppendSequence(element, sequence) {
+            for (var i = 0; i < sequence.length; i += 1) {
+                expect(element.append.calls[i].args[0].outerHTML)
+                    .toBe(sequence[i].outerHTML);
+            }
+        }
+    });
 });
