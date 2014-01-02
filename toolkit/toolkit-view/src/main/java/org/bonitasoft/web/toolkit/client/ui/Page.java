f1ae7220d070a727427d0b9c95a0a9953a666bf0
julienmege <julien.mege@bonitasoft.com>
2014-01-02 16:15:37 +0100

    BS-2945 copyright with 2013 instead of 2014 (Fix build issue)

--- toolkit/toolkit-view/src/main/java/org/bonitasoft/web/toolkit/client/ui/Page.java
+++ toolkit/toolkit-view/src/main/java/org/bonitasoft/web/toolkit/client/ui/Page.java
@@ -51,7 +51,7 @@ public abstract class Page extends View {
         super();
 
         // FIXME dont set footer when it is a quickdetails page
-        final Text footerTxt = new Text(String.format("Bonitasoft © 2014 %s.", _("All rights reserved")));
+        final Text footerTxt = new Text("Bonitasoft © 2014 " +  _("All rights reserved") + ".");
         footerTxt.getElement().setAttribute("id", "footer");
         super.setFooter(footerTxt);
     }
