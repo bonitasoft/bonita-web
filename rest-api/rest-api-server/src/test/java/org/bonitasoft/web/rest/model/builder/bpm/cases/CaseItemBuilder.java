d7ef387e27708b59d860a2cbb3d6ea4e8881fd60
ntith <nicolas.tith@bonitasoft.com>
2014-04-17 01:17:56 +0200

    fix test

--- rest-api/rest-api-server/src/test/java/org/bonitasoft/web/rest/model/builder/bpm/cases/CaseItemBuilder.java
+++ rest-api/rest-api-server/src/test/java/org/bonitasoft/web/rest/model/builder/bpm/cases/CaseItemBuilder.java
@@ -36,7 +36,7 @@ public class CaseItemBuilder {
         return this;
     }
 
-    public CaseItemBuilder withUserId(final long userId) {
+    public CaseItemBuilder withSubstituteUserId(final long userId) {
         this.userId = userId;
         return this;
     }
