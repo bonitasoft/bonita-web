16365d26e10dacb6ce23dfdcd74225b69a56c4cf
Vincent Elcrin <vincent.elcrin@bonitasoft.com>
2014-06-10 12:05:33 +0200

    Add angular yoeman boilderplate

---
+++ portaljs/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('portaljsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
