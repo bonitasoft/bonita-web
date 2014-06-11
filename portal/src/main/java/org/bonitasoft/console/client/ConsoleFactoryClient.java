0e3e7d83e131ec616a8f16342e71d7d1979737d9
Vincent Elcrin <vincent.elcrin@bonitasoft.com>
2014-06-11 12:10:00 +0200

    Change webapp structure to fit maven plugin

--- portal/src/main/java/org/bonitasoft/console/client/ConsoleFactoryClient.java
+++ portal/src/main/java/org/bonitasoft/console/client/ConsoleFactoryClient.java
@@ -103,7 +103,7 @@ public class ConsoleFactoryClient extends ApplicationFactoryClient {
         GWT.log("Current log user as access to :" + listAUthorizedTokens(AvailableTokens.tokens));
 
         if("angulariframe".equals(token)) {
-            return new AngularIFrameView("angulariframe", "#");
+            return new AngularIFrameView(token, "#");
         }
         if ("angularnative".equals(token)) {
             return new AngularNativeView("angularnative");
