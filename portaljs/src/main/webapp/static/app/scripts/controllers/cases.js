b663612eb013c6dceff3dbfa817dbdc5375a1cc6
Vincent Elcrin <vincent.elcrin@bonitasoft.com>
2014-06-12 18:38:14 +0200

    Add links to move between angular pages & gwt pages.

--- portaljs/src/main/webapp/static/app/scripts/controllers/cases.js
+++ portaljs/src/main/webapp/static/app/scripts/controllers/cases.js
@@ -3,6 +3,4 @@
 angular.module('portaljsApp')
   .controller('CasesCtrl', ['$scope', '$location', function ($scope, $location) {
     $scope.cases=[{'name' :'Case1'}, {'name' :'Case2'}];
-    $scope.goToUsersLink= '#'+$location.path() + '?_p=userlistingadmin&_f=enabledusers';
-
   }]);
