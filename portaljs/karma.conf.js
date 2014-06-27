ef73a0f3a2c44babe68da03290364685f3a12426
julien-reboul <julien.reboul@bonitasoft.com>
2014-06-27 16:13:10 +0200

    apply portaljs sp features on community

--- portaljs/karma.conf.js
+++ portaljs/karma.conf.js
@@ -2,6 +2,7 @@
 // http://karma-runner.github.io/0.10/config/configuration-file.html
 
 module.exports = function(config) {
+  'use strict';
   config.set({
     // base path, that will be used to resolve files and exclude
     basePath: '',
@@ -11,11 +12,22 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'main/assets/jquery/dist/jquery.js',
       'main/assets/angular/angular.js',
+      'main/assets/angular-cookies/angular-cookies.js',
+      'main/assets/angular-resource/angular-resource.js',
+      'main/assets/angular-route/angular-route.js',
+      'main/assets/angular-mocks/angular-mocks.js',
+      'main/assets/angular-ui-router/release/angular-ui-router.js',
+      'main/assets/angular-bootstrap/ui-bootstrap-tpls.js',
+      'main/assets/bootstrap/dist/js/bootstrap.js',
+      'main/assets/ng-grid/build/ng-grid.js',
+      'main/assets/jqueryui/ui/jquery-ui.js',
+      'main/assets/angular-gettext/dist/angular-gettext.js',
       'main/app.js',
-      'main/assets/angular-*/*.js',
-      'main/features/**/*.js',
+      'main/i18n.js',
       'main/common/**/*.js',
+      'main/features/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
     ],
@@ -43,7 +55,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
