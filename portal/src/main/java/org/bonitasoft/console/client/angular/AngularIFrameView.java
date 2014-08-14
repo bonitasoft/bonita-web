7c6c15277bd33a30ebf5cc7ab3faf9296e5f3fc2
Vincent Elcrin <vincent.elcrin@bonitasoft.com>
2014-08-14 16:32:01 +0200

    BS-9727, check session validity before loading angular page.

--- portal/src/main/java/org/bonitasoft/console/client/angular/AngularIFrameView.java
+++ portal/src/main/java/org/bonitasoft/console/client/angular/AngularIFrameView.java
@@ -7,6 +7,8 @@ import com.google.gwt.safehtml.shared.SafeUri;
 import com.google.gwt.user.client.ui.SimplePanel;
 import org.bonitasoft.console.client.user.cases.view.IFrameView;
 import org.bonitasoft.web.toolkit.client.ui.RawView;
+import org.bonitasoft.web.toolkit.client.ui.action.Action;
+import org.bonitasoft.web.toolkit.client.ui.action.CheckValidSessionBeforeAction;
 import org.bonitasoft.web.toolkit.client.ui.component.core.UiComponent;
 
 /**
@@ -30,9 +32,14 @@ public class AngularIFrameView extends RawView {
 
     @Override
     public void buildView() {
-        SimplePanel panel = new SimplePanel();
+        final SimplePanel panel = new SimplePanel();
         panel.setStyleName("body");
-        panel.add(new IFrameView(ROOT.contextualize(url)));
+        new CheckValidSessionBeforeAction(new Action() {
+            @Override
+            public void execute() {
+                panel.add(new IFrameView(ROOT.contextualize(url)));
+            }
+        }).execute();
         addBody(new UiComponent(panel));
         addClass("page");
     }
