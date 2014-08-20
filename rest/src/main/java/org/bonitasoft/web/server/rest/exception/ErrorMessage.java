16c47e274070ab80b9376b286c4f004716f1b8ce
Colin PUY <colin.puy@bonitasoft.com>
2014-08-20 16:51:49 +0200

    BS-9782, clean

--- rest/src/main/java/org/bonitasoft/web/server/rest/exception/ErrorMessage.java
+++ rest/src/main/java/org/bonitasoft/web/server/rest/exception/ErrorMessage.java
@@ -15,26 +15,11 @@ public class ErrorMessage {
     public String getType() {
         return type;
     }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
     public String getMessage() {
         return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 
     public int getStatus() {
         return status;
     }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-    
-    
 }
