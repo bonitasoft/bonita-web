1bba814023deb5ade81e7927e217e1d5eb9aa060
Colin PUY <colin.puy@bonitasoft.com>
2014-08-25 10:14:38 +0200

    BS-9664, change execute human task from PUT to POST

--- rest/src/main/java/org/bonitasoft/web/server/rest/resources/TaskResource.java
+++ rest/src/main/java/org/bonitasoft/web/server/rest/resources/TaskResource.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -45,7 +45,7 @@ public class TaskResource {
         }
     }
     
-    @PUT
+    @POST
     @Path("{taskId}/execute")
     public void executeTask(@PathParam("taskId") long taskId, List<Input> inputs) {
         try {
