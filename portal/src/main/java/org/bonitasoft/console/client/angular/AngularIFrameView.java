87248450d32f9c2a03ab85fb9e372264e8a41561
julien mege <julien.mege@bonitasoft.com>
2014-10-09 15:58:55 +0200

    format some code + organize import

--- portal/src/main/java/org/bonitasoft/console/client/angular/AngularIFrameView.java
+++ portal/src/main/java/org/bonitasoft/console/client/angular/AngularIFrameView.java
@@ -1,26 +1,24 @@
 package org.bonitasoft.console.client.angular;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.safehtml.client.SafeHtmlTemplates;
-import com.google.gwt.safehtml.shared.SafeHtml;
-import com.google.gwt.safehtml.shared.SafeUri;
-import com.google.gwt.user.client.ui.SimplePanel;
 import org.bonitasoft.console.client.user.cases.view.IFrameView;
 import org.bonitasoft.web.toolkit.client.ui.RawView;
 import org.bonitasoft.web.toolkit.client.ui.action.Action;
 import org.bonitasoft.web.toolkit.client.ui.action.CheckValidSessionBeforeAction;
 import org.bonitasoft.web.toolkit.client.ui.component.core.UiComponent;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.SimplePanel;
+
 /**
  * @author Vincent Elcrin
  */
 public class AngularIFrameView extends RawView {
 
-    private String url;
+    private final String url;
 
     private static final AngularResourceRoot ROOT = GWT.create(AngularResourceRoot.class);
 
-    public AngularIFrameView(String token, String url) {
+    public AngularIFrameView(final String token, final String url) {
         setToken(token);
         this.url = url;
     }
