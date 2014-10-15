550799d3682bff8698ff1be313aafde294b3afe2
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2014-10-15 16:25:00 +0200

    BS-10372, fix redirection mechanism (still needs adjustments)

--- server/src/main/java/org/bonitasoft/web/rest/server/BonitaRestletAPIServlet.java
+++ server/src/main/java/org/bonitasoft/web/rest/server/BonitaRestletAPIServlet.java
@@ -1,78 +1,66 @@
 package org.bonitasoft.web.rest.server;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-
 import org.bonitasoft.web.rest.server.api.bpm.flownode.TaskVariableResource;
 import org.bonitasoft.web.rest.server.api.bpm.flownode.TimerEventTriggerResource;
 import org.restlet.Application;
 import org.restlet.Context;
-import org.restlet.Request;
-import org.restlet.Response;
 import org.restlet.Restlet;
-import org.restlet.ext.servlet.ServletUtils;
 import org.restlet.routing.Router;
 
 public class BonitaRestletAPIServlet extends Application {
 
-    protected static List<String> OLD_APIs = Arrays.asList(
-            "/bpm/activity",
-            "/bpm/actor",
-            "/bpm/actorMember",
-            "/bpm/archivedActivity",
-            "/bpm/archivedCase",
-            "/bpm/archivedComment",
-            "/bpm/archivedConnectorInstance",
-            "/bpm/archiveddocument",
-            "/bpm/archivedFlowNode",
-            "/bpm/archivedHumanTask",
-            "/bpm/archivedTask",
-            "/bpm/archivedUserTask",
-            "/bpm/case",
-            "/bpm/caseDocument",
-            "/bpm/caseVariable",
-            "/bpm/category",
-            "/bpm/comment",
-            "/bpm/connectorInstance",
-            "/bpm/delegation",
-            "/bpm/document",
-            "/bpm/document",
-            "/bpm/flowNode",
-            "/bpm/hiddenUserTask",
-            "/bpm/humanTask",
-            "/bpm/process",
-            "/bpm/processCategory",
-            "/bpm/processConnector",
-            "/bpm/processConnectorDependency",
-            "/bpm/processResolutionProblem",
-            "/bpm/task",
-            "/bpm/userTask",
-            "/customuserinfo/definition",
-            "/customuserinfo/user",
-            "/customuserinfo/value",
-            "/identity/group",
-            "/identity/membership",
-            "/identity/personalcontactdata",
-            "/identity/professionalcontactdata",
-            "/identity/role",
-            "/identity/user",
-            "/platform/platform",
-            "/portal/profile",
-            "/portal/profileEntry",
-            "/portal/profileMember",
-            "/system/i18nlocale",
-            "/system/i18ntranslation",
-            "/system/session",
-            "/userXP/profile",
-            "/userXP/profileEntry",
-            "/userXP/profileMember"
-            );
+    //    protected static List<String> OLD_APIs = Arrays.asList(
+    //            "/bpm/activity",
+    //            "/bpm/actor",
+    //            "/bpm/actorMember",
+    //            "/bpm/archivedActivity",
+    //            "/bpm/archivedCase",
+    //            "/bpm/archivedComment",
+    //            "/bpm/archivedConnectorInstance",
+    //            "/bpm/archiveddocument",
+    //            "/bpm/archivedFlowNode",
+    //            "/bpm/archivedHumanTask",
+    //            "/bpm/archivedTask",
+    //            "/bpm/archivedUserTask",
+    //            "/bpm/case",
+    //            "/bpm/caseDocument",
+    //            "/bpm/caseVariable",
+    //            "/bpm/category",
+    //            "/bpm/comment",
+    //            "/bpm/connectorInstance",
+    //            "/bpm/delegation",
+    //            "/bpm/document",
+    //            "/bpm/document",
+    //            "/bpm/flowNode",
+    //            "/bpm/hiddenUserTask",
+    //            "/bpm/humanTask",
+    //            "/bpm/process",
+    //            "/bpm/processCategory",
+    //            "/bpm/processConnector",
+    //            "/bpm/processConnectorDependency",
+    //            "/bpm/processResolutionProblem",
+    //            "/bpm/task",
+    //            "/bpm/userTask",
+    //            "/customuserinfo/definition",
+    //            "/customuserinfo/user",
+    //            "/customuserinfo/value",
+    //            "/identity/group",
+    //            "/identity/membership",
+    //            "/identity/personalcontactdata",
+    //            "/identity/professionalcontactdata",
+    //            "/identity/role",
+    //            "/identity/user",
+    //            "/platform/platform",
+    //            "/portal/profile",
+    //            "/portal/profileEntry",
+    //            "/portal/profileMember",
+    //            "/system/i18nlocale",
+    //            "/system/i18ntranslation",
+    //            "/system/session",
+    //            "/userXP/profile",
+    //            "/userXP/profileEntry",
+    //            "/userXP/profileMember"
+    //            );
 
     /**
      * Creates a root Restlet that will receive all incoming calls.
@@ -84,60 +72,8 @@ public class BonitaRestletAPIServlet extends Application {
 
         router.attach("/bpm/taskVariable", TaskVariableResource.class);
         router.attach("/bpm/timerEventTrigger", TimerEventTriggerResource.class);
+
         return router;
-    }
-
-    @Override
-    public void handle(final Request request, final Response response) {
-        // Old rest APIs:
-        if (isV1RestApi(getHttpRequest(request).getPathInfo())) {
-            try {
-                //ServletUtils.getResponse(response).sendRedirect("/APIv1");
-                final HttpServletRequest httpRequest = getHttpRequest(request);
-                final ServletResponse httpResponse = getHttpResponse(response);
-                httpRequest.setCharacterEncoding("UTF-8");
-                httpResponse.setCharacterEncoding("UTF-8");
-                final RequestDispatcher rd = httpRequest.getRequestDispatcher("/APIv1");
-                rd.include(httpRequest, httpResponse);
-            } catch (final IOException e) {
-                e.printStackTrace();
-            } catch (final ServletException e) {
-                e.printStackTrace();
-            }
-
-            return;
-        }
-        // New Restlet APIs:
-        super.handle(request, response);
-    }
-
-    private boolean isV1RestApi(final String pathInfo) {
-        for (final String url : OLD_APIs) {
-            if (pathInfo.startsWith(url)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * gets the servlet response from the restlet response
-     *
-     * @param response
-     * @return ServletResponse
-     */
-    private ServletResponse getHttpResponse(final Response response) {
-        return ServletUtils.getResponse(response);
-    }
-
-    /**
-     * gets the httpRequest from the restlet request
-     *
-     * @param request
-     * @return HttpServletRequest
-     */
-    private HttpServletRequest getHttpRequest(final Request request) {
-        return ServletUtils.getRequest(request);
     }
 
 }
