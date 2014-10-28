e33308f79f35953f4b2a9bd2adf6d556c04fd21f
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2014-10-28 11:15:09 +0100

    renamed BonitaRestletAPIServlet into BonitaRestletApplication

--- server/src/main/java/org/bonitasoft/web/rest/server/BonitaRestletApplication.java
+++ server/src/main/java/org/bonitasoft/web/rest/server/BonitaRestletApplication.java
@@ -12,7 +12,7 @@ import org.restlet.Restlet;
 import org.restlet.engine.Engine;
 import org.restlet.routing.Router;
 
-public class BonitaRestletAPIServlet extends Application {
+public class BonitaRestletApplication extends Application {
 
     /**
      * Creates a root Restlet that will receive all incoming calls.
