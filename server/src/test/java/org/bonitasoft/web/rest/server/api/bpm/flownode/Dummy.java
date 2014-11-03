510c9428854ae1d6f4075a132461c47c5059c389
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2014-11-03 17:48:02 +0100

    BS-11290, support UTF-8 encoded characters in inputs and outputs in Restlet resources

--- server/src/test/java/org/bonitasoft/web/rest/server/api/bpm/flownode/Dummy.java
+++ server/src/test/java/org/bonitasoft/web/rest/server/api/bpm/flownode/Dummy.java
@@ -2,13 +2,14 @@ package org.bonitasoft.web.rest.server.api.bpm.flownode;
 
 public class Dummy {
 
-    private String field = "procVarJapanカキクケコ";
+    private final String field;
+
+    public Dummy(final String fieldValue) {
+        field = fieldValue;
+    }
 
     public String getField() {
         return field;
     }
 
-    public void setField(final String field) {
-        this.field = field;
-    }
 }
