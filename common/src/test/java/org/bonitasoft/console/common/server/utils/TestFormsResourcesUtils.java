03dd557f36682819b5ccf8cc7ea4a52bb258d84f
anthony <anthony.birembaut@bonitasoft.com>
2014-11-19 16:22:43 +0100

    BS-11328 - Fix classloader - tests

--- common/src/test/java/org/bonitasoft/console/common/server/utils/TestFormsResourcesUtils.java
+++ common/src/test/java/org/bonitasoft/console/common/server/utils/TestFormsResourcesUtils.java
@@ -1,11 +1,8 @@
 package org.bonitasoft.console.common.server.utils;
 
-import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
-
-import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
 import org.bonitasoft.engine.api.ProcessAPI;
@@ -72,20 +69,43 @@ public class TestFormsResourcesUtils extends AbstractJUnitTest {
         final BusinessArchive businessArchive = new BusinessArchiveBuilder().createNewBusinessArchive()
                 .setProcessDefinition(processDefinitionBuilder.done())
                 .addExternalResource(new BarResource("forms/lib/messages.jar", IOUtils.toByteArray(this.getClass().getResourceAsStream("/messages.jar"))))
-                .addExternalResource(new BarResource("forms/messages.jar", IOUtils.toByteArray(this.getClass().getResourceAsStream("/messages.jar"))))
-                .addClasspathResource(new BarResource("messages.jar", IOUtils.toByteArray(this.getClass().getResourceAsStream("/messages.jar"))))
                 .done();
 
         final ProcessAPI processAPI = TenantAPIAccessor.getProcessAPI(getSession());
         final ProcessDefinition processDefinition = processAPI.deploy(businessArchive);
         final ProcessDeploymentInfo processDeploymentInfo = processAPI.getProcessDeploymentInfo(processDefinition.getId());
 
-        final Map<String, byte[]> resources = processAPI.getProcessResources(processDefinition.getId(), ".*");
-        assertThat(resources.isEmpty(), is(false));
-
         FormsResourcesUtils.retrieveApplicationFiles(getSession(), processDeploymentInfo.getProcessId(), processDeploymentInfo.getDeploymentDate());
 
         final FormsResourcesUtils formsResourcesUtilsInstance = new FormsResourcesUtils();
         assertNotNull(formsResourcesUtilsInstance.getProcessClassLoader(getSession(), processDefinition.getId()));
+    }
+
+    @Test
+    public void testGetProcessClassLoaderReturnSameClassLoaderWhenCalledSeveralTimes() throws Exception {
+        final ProcessDefinitionBuilder processDefinitionBuilder = new ProcessDefinitionBuilder();
+        processDefinitionBuilder.createNewInstance("Test process", "3.0");
+        processDefinitionBuilder.addActor("actorName").addUserTask("taskName", "actorName");
+        // upload process archive
+        final BusinessArchive businessArchive = new BusinessArchiveBuilder().createNewBusinessArchive()
+                .setProcessDefinition(processDefinitionBuilder.done())
+                .addExternalResource(new BarResource("forms/lib/messages.jar", IOUtils.toByteArray(this.getClass().getResourceAsStream("/messages.jar"))))
+                .done();
+
+        final ProcessAPI processAPI = TenantAPIAccessor.getProcessAPI(getSession());
+        final ProcessDefinition processDefinition = processAPI.deploy(businessArchive);
+        final ProcessDeploymentInfo processDeploymentInfo = processAPI.getProcessDeploymentInfo(processDefinition.getId());
+
+        FormsResourcesUtils.retrieveApplicationFiles(getSession(), processDeploymentInfo.getProcessId(), processDeploymentInfo.getDeploymentDate());
+
+        final FormsResourcesUtils formsResourcesUtilsInstance = new FormsResourcesUtils();
+        final ClassLoader processClassloader = formsResourcesUtilsInstance.getProcessClassLoader(getSession(), processDefinition.getId());
+        assertNotNull(processClassloader);
+
+        final FormsResourcesUtils formsResourcesUtilsInstance2 = new FormsResourcesUtils();
+        final ClassLoader processClassloader2 = formsResourcesUtilsInstance2.getProcessClassLoader(getSession(), processDefinition.getId());
+        assertNotNull(processClassloader2);
+
+        assertEquals(processClassloader, processClassloader2);
     }
 }
