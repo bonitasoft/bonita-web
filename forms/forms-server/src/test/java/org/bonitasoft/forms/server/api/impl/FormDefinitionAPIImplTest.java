541041942a69a2db915d19c2f36627d0e1e2c06f
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2014-12-12 14:22:45 +0100

    BS-11711, fix ClassNotFoundException when transient data use a business data

--- forms/forms-server/src/test/java/org/bonitasoft/forms/server/api/impl/FormDefinitionAPIImplTest.java
+++ forms/forms-server/src/test/java/org/bonitasoft/forms/server/api/impl/FormDefinitionAPIImplTest.java
@@ -14,7 +14,10 @@
  */
 package org.bonitasoft.forms.server.api.impl;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyListOf;
+import static org.mockito.Matchers.anyMapOf;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doReturn;
@@ -23,16 +26,22 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 import org.bonitasoft.engine.session.APISession;
+import org.bonitasoft.forms.client.model.Expression;
 import org.bonitasoft.forms.client.model.FormPage;
 import org.bonitasoft.forms.client.model.HtmlTemplate;
+import org.bonitasoft.forms.client.model.TransientData;
 import org.bonitasoft.forms.server.accessor.impl.EngineApplicationFormDefAccessorImpl;
 import org.bonitasoft.forms.server.accessor.impl.util.FormCacheUtil;
+import org.bonitasoft.forms.server.provider.FormServiceProvider;
 import org.bonitasoft.forms.server.provider.impl.util.FormServiceProviderUtil;
 import org.junit.Assert;
 import org.junit.Before;
@@ -49,6 +58,12 @@ import org.w3c.dom.Document;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class FormDefinitionAPIImplTest {
+
+    private static final String EXPRESSION_NAME = "expressionName";
+
+    private static final String TRANSIENT_VALUE = "transientValue";
+
+    private static final String TRANSIENT_DATA_NAME = "transientDataName";
 
     private static final long TENANT_ID = 1L;
 
@@ -68,25 +83,65 @@ public class FormDefinitionAPIImplTest {
     @Mock
     APISession apiSession;
 
+    @Mock
+    private Expression transientDataExpression;
+
+    @Mock
+    FormServiceProvider formServiceProvider;
+
+    @Mock
+    private Map<String, Serializable> evaluatedExpressions;
+
+    @Mock
+    private FormCacheUtil formCacheUtil;
+
+    @Mock
+    private Serializable evaluatedValue;
+
+    FormDefinitionAPIImpl formDefinitionAPI;
+
+    private class ParentPojo implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+        private String data;
+
+        public void setData(final String data) {
+            this.data = data;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+    }
+
+    private class ChildPojo extends ParentPojo {
+
+        public ChildPojo() {
+            super();
+        }
+    }
+
     @Before
-    public void before() {
+    public void before() throws Exception {
         System.setProperty("bonita.home", "target/bonita-home/bonita");
         doReturn(urlContext).when(context).get(FormServiceProviderUtil.URL_CONTEXT);
         doReturn(apiSession).when(context).get(FormServiceProviderUtil.API_SESSION);
         doReturn(TENANT_ID).when(apiSession).getTenantId();
+
+        //        final FormCacheUtil formCacheUtil = mock(FormCacheUtil.class);
+        formDefinitionAPI = spy(new FormDefinitionAPIImpl(1, document, formCacheUtil, deployementDate, Locale.ENGLISH.toString()));
     }
 
     @Test
     public void getFormPageFromCache() throws Exception {
         // Given
-        final FormCacheUtil formCacheUtil = mock(FormCacheUtil.class);
-        final FormDefinitionAPIImpl api = spy(new FormDefinitionAPIImpl(1, document, formCacheUtil, deployementDate, Locale.ENGLISH.toString()));
-        doReturn(mock(EngineApplicationFormDefAccessorImpl.class)).when(api).getApplicationFormDefinition(anyString(), eq(context));
-        doReturn(mock(HtmlTemplate.class)).when(api).getPageLayout(anyString(), any(Date.class), eq(context));
-        final FormPage formPageFirstCall = api.getFormPage(FORM_ID, PAGE_ID, context);
+        doReturn(mock(EngineApplicationFormDefAccessorImpl.class)).when(formDefinitionAPI).getApplicationFormDefinition(anyString(), eq(context));
+        doReturn(mock(HtmlTemplate.class)).when(formDefinitionAPI).getPageLayout(anyString(), any(Date.class), eq(context));
+        final FormPage formPageFirstCall = formDefinitionAPI.getFormPage(FORM_ID, PAGE_ID, context);
 
         // When
-        final FormPage formPage = api.getFormPage(FORM_ID, PAGE_ID, context);
+        final FormPage formPage = formDefinitionAPI.getFormPage(FORM_ID, PAGE_ID, context);
 
         // Then
         Assert.assertNotNull(formPage);
@@ -94,4 +149,99 @@ public class FormDefinitionAPIImplTest {
         verify(formCacheUtil, times(1)).storePage(FORM_ID, Locale.ENGLISH.toString(), deployementDate, formPageFirstCall);
         Assert.assertEquals("processPage1", formPage.getPageId());
     }
+
+    @Test
+    public void getTransientDataContextWithBdm() throws Exception {
+        final ChildPojo childPojo = new ChildPojo();
+        doReturn(true).when(formDefinitionAPI).isProxyfiedBdmValue(any(Serializable.class));
+        doReturn(EXPRESSION_NAME).when(transientDataExpression).getName();
+        doReturn(formServiceProvider).when(formDefinitionAPI).getFormServiceProvider();
+        doReturn(evaluatedExpressions).when(formServiceProvider).resolveExpressions(anyListOf(Expression.class), anyMapOf(String.class, Object.class));
+        doReturn(childPojo).when(evaluatedExpressions).get(EXPRESSION_NAME);
+
+        final List<TransientData> transientDataList = new ArrayList<TransientData>();
+        final TransientData transientData = new TransientData(TRANSIENT_DATA_NAME, ParentPojo.class.getName(), transientDataExpression);
+        transientDataList.add(transientData);
+
+        // When
+        final Map<String, Serializable> transientDataContext = formDefinitionAPI.getTransientDataContext(transientDataList, Locale.ENGLISH, context);
+
+        // Then
+        assertThat(transientDataContext).containsEntry(TRANSIENT_DATA_NAME, childPojo);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getTransientDataContextWithBadReturnClass() throws Exception {
+        // given
+        doReturn(EXPRESSION_NAME).when(transientDataExpression).getName();
+        doReturn(formServiceProvider).when(formDefinitionAPI).getFormServiceProvider();
+        doReturn(evaluatedExpressions).when(formServiceProvider).resolveExpressions(anyListOf(Expression.class), anyMapOf(String.class, Object.class));
+        doReturn(new Date()).when(evaluatedExpressions).get(EXPRESSION_NAME);
+
+        final List<TransientData> transientDataList = new ArrayList<TransientData>();
+        final TransientData transientData = new TransientData(TRANSIENT_DATA_NAME, String.class.getName(), transientDataExpression);
+        transientDataList.add(transientData);
+
+        // When
+        formDefinitionAPI.getTransientDataContext(transientDataList, Locale.ENGLISH, context);
+
+        // Then exception
+    }
+
+    @Test
+    public void getTransientDataContextWithValue() throws Exception {
+        // Given
+        doReturn(EXPRESSION_NAME).when(transientDataExpression).getName();
+        doReturn(formServiceProvider).when(formDefinitionAPI).getFormServiceProvider();
+        doReturn(evaluatedExpressions).when(formServiceProvider).resolveExpressions(anyListOf(Expression.class), anyMapOf(String.class, Object.class));
+        doReturn(TRANSIENT_VALUE).when(evaluatedExpressions).get(EXPRESSION_NAME);
+
+        final List<TransientData> transientDataList = new ArrayList<TransientData>();
+        final TransientData transientData = new TransientData(TRANSIENT_DATA_NAME, String.class.getName(), transientDataExpression);
+        transientDataList.add(transientData);
+
+        // When
+        final Map<String, Serializable> transientDataContext = formDefinitionAPI.getTransientDataContext(transientDataList, Locale.ENGLISH, context);
+
+        // Then
+        assertThat(transientDataContext).containsEntry(TRANSIENT_DATA_NAME, TRANSIENT_VALUE);
+
+    }
+
+    @Test
+    public void getTransientDataContextWithNullValue() throws Exception {
+        // Given
+        doReturn(EXPRESSION_NAME).when(transientDataExpression).getName();
+        doReturn(formServiceProvider).when(formDefinitionAPI).getFormServiceProvider();
+        doReturn(evaluatedExpressions).when(formServiceProvider).resolveExpressions(anyListOf(Expression.class), anyMapOf(String.class, Object.class));
+        doReturn(null).when(evaluatedExpressions).get(EXPRESSION_NAME);
+
+        final List<TransientData> transientDataList = new ArrayList<TransientData>();
+        final TransientData transientData = new TransientData(TRANSIENT_DATA_NAME, String.class.getName(), transientDataExpression);
+        transientDataList.add(transientData);
+
+        // When
+        final Map<String, Serializable> transientDataContext = formDefinitionAPI.getTransientDataContext(transientDataList, Locale.ENGLISH, context);
+
+        // Then
+        assertThat(transientDataContext).containsEntry(TRANSIENT_DATA_NAME, null);
+
+    }
+
+    @Test
+    public void getTransientDataContextWithoutExpression() throws Exception {
+        // Given
+        final List<TransientData> transientDataList = new ArrayList<TransientData>();
+        final TransientData transientData = new TransientData();
+        transientData.setName(TRANSIENT_DATA_NAME);
+        transientDataList.add(transientData);
+
+        // When
+        final Map<String, Serializable> transientDataContext = formDefinitionAPI.getTransientDataContext(transientDataList, Locale.ENGLISH, context);
+
+        // Then
+        assertThat(transientDataContext).containsEntry(TRANSIENT_DATA_NAME, null);
+
+    }
+
 }
