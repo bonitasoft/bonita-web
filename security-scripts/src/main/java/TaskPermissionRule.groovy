98d852c7d8f719c594ff72317327a015792944b6
Elias Ricken de Medeiros <elias.ricken@bonitasoft.com>
2015-01-23 16:53:17 +0100

    BS-12223, fix long parsing

--- security-scripts/src/main/java/TaskPermissionRule.groovy
+++ security-scripts/src/main/java/TaskPermissionRule.groovy
@@ -90,11 +90,11 @@ class TaskPermissionRule implements PermissionRule {
             logger.debug("FilterOnUser or FilterOnAssignUser")
             return true
         } else if (filters.containsKey("parentTaskId")) {
-            def long parentTaskId = Long.parseLong(filters.get("parentTaskId"))
+            def parentTaskId = filters.get("parentTaskId")
             try {
                 return isTaskAccessible(processAPI, parentTaskId, currentUserId, userName, logger)
             } catch (NotFoundException e) {
-                return isArchivedFlowNodeAccessible(processAPI, filters.get("parentTaskId"), currentUserId)
+                return isArchivedFlowNodeAccessible(processAPI, parentTaskId, currentUserId)
             }
         } else if (filters.containsKey("processId")) {
             def long processId = Long.valueOf(filters.get("processId"))
