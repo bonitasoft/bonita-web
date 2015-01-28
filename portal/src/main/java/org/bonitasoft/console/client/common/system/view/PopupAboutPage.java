4f1fa2bb8a4c9d7677fc51a9c18914f9ed08217c
anthony <anthony.birembaut@bonitasoft.com>
2015-01-28 19:14:32 +0100

    BS-11635 remove portal footer

--- portal/src/main/java/org/bonitasoft/console/client/common/system/view/PopupAboutPage.java
+++ portal/src/main/java/org/bonitasoft/console/client/common/system/view/PopupAboutPage.java
@@ -5,27 +5,28 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.console.client.common.system.view;
 
+import static org.bonitasoft.web.toolkit.client.common.i18n.AbstractI18n._;
+
 import org.bonitasoft.web.toolkit.client.Session;
 import org.bonitasoft.web.toolkit.client.common.session.SessionItem;
 import org.bonitasoft.web.toolkit.client.ui.Page;
 import org.bonitasoft.web.toolkit.client.ui.component.Definition;
-
-import static org.bonitasoft.web.toolkit.client.common.i18n.AbstractI18n._;
+import org.bonitasoft.web.toolkit.client.ui.component.Paragraph;
 
 /**
  * @author Julien Mege
- * 
+ *
  */
 public class PopupAboutPage extends Page {
 
@@ -39,13 +40,21 @@ public class PopupAboutPage extends Page {
     @Override
     public void buildView() {
         addBody(new Definition(_("Version") + " : ", getVersion()));
+        addBody(new Paragraph(getCopyright()));
     }
-    
+
     /**
-     * get version from html host page in meta tag named version.
+     * get version from session.
      */
-    private String getVersion() {
+    protected String getVersion() {
         return Session.getParameter(SessionItem.ATTRIBUTE_VERSION);
+    }
+
+    /**
+     * get copyright from session.
+     */
+    protected String getCopyright() {
+        return Session.getParameter(SessionItem.ATTRIBUTE_COPYRIGHT);
     }
 
     @Override
