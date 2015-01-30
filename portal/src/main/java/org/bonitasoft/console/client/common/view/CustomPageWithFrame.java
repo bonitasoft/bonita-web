0739b70f4b84525c67370438bcebb82d79fc4756
anthony <anthony.birembaut@bonitasoft.com>
2015-01-30 18:55:49 +0100

    fix copyright in header

--- portal/src/main/java/org/bonitasoft/console/client/common/view/CustomPageWithFrame.java
+++ portal/src/main/java/org/bonitasoft/console/client/common/view/CustomPageWithFrame.java
@@ -1,11 +1,17 @@
-/*******************************************************************************
+/**
  * Copyright (C) 2014 BonitaSoft S.A.
- * BonitaSoft is a trademark of BonitaSoft SA.
- * This software file is BONITASOFT CONFIDENTIAL. Not For Distribution.
- * For commercial licensing information, contact:
- * BonitaSoft, 32 rue Gustave Eiffel – 38000 Grenoble
- * or BonitaSoft US, 51 Federal Street, Suite 305, San Francisco, CA 94107
- *******************************************************************************/
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.bonitasoft.console.client.common.view;
 
 import java.util.List;
@@ -24,7 +30,7 @@ import com.google.gwt.user.client.Element;
 
 /**
  * @author Anthony Birembaut
- * 
+ *
  */
 public class CustomPageWithFrame extends Page {
 
@@ -63,7 +69,7 @@ public class CustomPageWithFrame extends Page {
 
     /**
      * We don't need any header and it screw up the page's size.
-     * 
+     *
      * @param header
      * @return
      */
@@ -74,12 +80,12 @@ public class CustomPageWithFrame extends Page {
 
     /**
      * We don't need any footer
-     * 
+     *
      * @param footer
      * @return
      */
     @Override
-    protected List<Element> makeFooterElements(Container<AbstractComponent> footer) {
+    protected List<Element> makeFooterElements(final Container<AbstractComponent> footer) {
         return null;
     }
 
--- server/src/main/java/org/bonitasoft/web/rest/server/api/applicationpage/APIApplicationDataStoreFactory.java
+++ server/src/main/java/org/bonitasoft/web/rest/server/api/applicationpage/APIApplicationDataStoreFactory.java
@@ -1,13 +1,18 @@
-/*******************************************************************************
+/**
  * Copyright (C) 2009, 2013 BonitaSoft S.A.
- * BonitaSoft is a trademark of BonitaSoft SA.
- * This software file is BONITASOFT CONFIDENTIAL. Not For Distribution.
- * For commercial licensing information, contact:
- * BonitaSoft, 32 rue Gustave Eiffel – 38000 Grenoble
- * or BonitaSoft US, 51 Federal Street, Suite 305, San Francisco, CA 94107
- *******************************************************************************/
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.bonitasoft.web.rest.server.api.applicationpage;
-
 import org.bonitasoft.console.common.server.preferences.constants.WebBonitaConstantsUtils;
 import org.bonitasoft.engine.session.APISession;
 import org.bonitasoft.web.rest.server.datastore.application.ApplicationDataStore;
@@ -18,6 +23,7 @@ import org.bonitasoft.web.rest.server.datastore.page.PageDatastore;
 import org.bonitasoft.web.rest.server.datastore.page.PageDatastoreFactory;
 import org.bonitasoft.web.rest.server.engineclient.EngineAPIAccessor;
 import org.bonitasoft.web.toolkit.client.common.exception.api.APIException;
+
 
 public class APIApplicationDataStoreFactory {
 
