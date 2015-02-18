43c04d10ba0865e1caa3038be41129a798ef0f28
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2015-02-18 15:06:02 +0100

    BS-12451, move BDM jar to community form classloader

--- common/src/test/java/org/bonitasoft/console/common/server/utils/FormsResourcesUtilsIT.java
+++ common/src/test/java/org/bonitasoft/console/common/server/utils/FormsResourcesUtilsIT.java
@@ -1,12 +1,18 @@
 package org.bonitasoft.console.common.server.utils;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
 
 import org.apache.commons.io.IOUtils;
+import org.bonitasoft.engine.api.LoginAPI;
 import org.bonitasoft.engine.api.ProcessAPI;
 import org.bonitasoft.engine.api.TenantAPIAccessor;
+import org.bonitasoft.engine.api.TenantAdministrationAPI;
+import org.bonitasoft.engine.bdm.BusinessObjectModelConverter;
+import org.bonitasoft.engine.bdm.model.BusinessObject;
+import org.bonitasoft.engine.bdm.model.BusinessObjectModel;
+import org.bonitasoft.engine.bdm.model.field.FieldType;
+import org.bonitasoft.engine.bdm.model.field.SimpleField;
 import org.bonitasoft.engine.bpm.bar.BarResource;
 import org.bonitasoft.engine.bpm.bar.BusinessArchive;
 import org.bonitasoft.engine.bpm.bar.BusinessArchiveBuilder;
@@ -21,6 +27,8 @@ import org.bonitasoft.test.toolkit.organization.TestUserFactory;
 import org.junit.Test;
 
 public class FormsResourcesUtilsIT extends AbstractJUnitTest {
+
+    private static final String PERSON_QUALIFIED_NAME = "com.company.pojo.Person";
 
     protected APISession getSession() {
         return getInitiator().getSession();
@@ -107,5 +115,72 @@ public class FormsResourcesUtilsIT extends AbstractJUnitTest {
         assertNotNull(processClassloader2);
 
         assertEquals(processClassloader, processClassloader2);
+    }
+
+    @Test
+    public void testGetProcessClassLoaderReturnSameClassLoaderWhenCalledSeveralTimesWithBDM() throws Exception {
+
+        final LoginAPI loginAPI = TenantAPIAccessor.getLoginAPI();
+        APISession technicalUserSession = loginAPI.login(TenantsManagementUtils.getTechnicalUserUsername(),
+                TenantsManagementUtils.getTechnicalUserPassword());
+        TenantAdministrationAPI tenantAdministrationAPI = TenantAPIAccessor.getTenantAdministrationAPI(technicalUserSession);
+        final BusinessObjectModelConverter converter = new BusinessObjectModelConverter();
+        final byte[] zip = converter.zip(createBusinessObjectModel());
+        tenantAdministrationAPI.pause();
+        tenantAdministrationAPI.cleanAndUninstallBusinessDataModel();
+        tenantAdministrationAPI.installBusinessDataModel(zip);
+        tenantAdministrationAPI.resume();
+        loginAPI.logout(technicalUserSession);
+
+        try {
+            final ProcessDefinitionBuilder processDefinitionBuilder = new ProcessDefinitionBuilder();
+            processDefinitionBuilder.createNewInstance("Test process", "4.0");
+            processDefinitionBuilder.addActor("actorName").addUserTask("taskName", "actorName");
+            // upload process archive
+            final BusinessArchive businessArchive = new BusinessArchiveBuilder().createNewBusinessArchive()
+                    .setProcessDefinition(processDefinitionBuilder.done())
+                    .addExternalResource(new BarResource("forms/lib/messages.jar", IOUtils.toByteArray(this.getClass().getResourceAsStream("/messages.jar"))))
+                    .done();
+
+            final ProcessAPI processAPI = TenantAPIAccessor.getProcessAPI(getSession());
+            final ProcessDefinition processDefinition = processAPI.deploy(businessArchive);
+            final ProcessDeploymentInfo processDeploymentInfo = processAPI.getProcessDeploymentInfo(processDefinition.getId());
+
+            FormsResourcesUtils.retrieveApplicationFiles(getSession(), processDeploymentInfo.getProcessId(), processDeploymentInfo.getDeploymentDate());
+
+            final FormsResourcesUtils formsResourcesUtilsInstance = new FormsResourcesUtils();
+            final ClassLoader processClassloader = formsResourcesUtilsInstance.getProcessClassLoader(getSession(), processDefinition.getId());
+
+            assertThat(processClassloader).as("should retrieve class loader").isNotNull();
+            assertThat(processClassloader.loadClass(PERSON_QUALIFIED_NAME)).as("should load BDM class").isNotNull();
+
+            final FormsResourcesUtils formsResourcesUtilsInstance2 = new FormsResourcesUtils();
+            final ClassLoader processClassloader2 = formsResourcesUtilsInstance2.getProcessClassLoader(getSession(), processDefinition.getId());
+
+            assertThat(processClassloader2).as("should retrieve class loader").isNotNull().isEqualTo(processClassloader2);
+            assertThat(processClassloader2.loadClass(PERSON_QUALIFIED_NAME)).as("should load BDM class").isNotNull();
+
+        } finally {
+            technicalUserSession = loginAPI.login(TenantsManagementUtils.getTechnicalUserUsername(),
+                    TenantsManagementUtils.getTechnicalUserPassword());
+            tenantAdministrationAPI = TenantAPIAccessor.getTenantAdministrationAPI(technicalUserSession);
+            tenantAdministrationAPI.pause();
+            tenantAdministrationAPI.uninstallBusinessDataModel();
+            tenantAdministrationAPI.resume();
+            loginAPI.logout(technicalUserSession);
+        }
+    }
+
+    protected BusinessObjectModel createBusinessObjectModel() {
+        final BusinessObjectModel businessObjectModel = new BusinessObjectModel();
+        final BusinessObject person = new BusinessObject();
+        person.setQualifiedName(PERSON_QUALIFIED_NAME);
+        final SimpleField name = new SimpleField();
+        name.setName("name");
+        name.setType(FieldType.STRING);
+        name.setLength(Integer.valueOf(10));
+        person.addField(name);
+        businessObjectModel.addBusinessObject(person);
+        return businessObjectModel;
     }
 }
