139d159c50ef95b8aa7d6906fe25d8e1c3542411
ntith <nicolas.tith@bonitasoft.com>
2015-02-24 10:54:02 +0100

    BS-11438 Cannot Import a file with IE9 (IE9 put the servlet return in <pre> object) : change error message while uploader is not handle

--- portal/src/main/webapp/portal/scripts/includes/autouploader.js
+++ portal/src/main/webapp/portal/scripts/includes/autouploader.js
@@ -173,7 +173,7 @@ $(function () {
             if (file.code == -601) {
             	addError(input.getOption('text.extensionerror'));
             } else if (file.code == -500) {
-            	addError('Our uploader doesn\'t support your browser. Support for:  html5, flash, silverlight');
+            	addError('The specified file cannot be uploaded. Add plugin (like flashplayer or silverlight) to your browser to handle this type of file, then try the upload again.');
             } else {
             	addError(file.message);
             }
