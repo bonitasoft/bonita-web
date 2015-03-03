acf19b71f04c4cac39e9ecbfc9afe9b340758f1b
anthony <anthony.birembaut@bonitasoft.com>
2015-03-03 14:13:35 +0100

    BS-12482 - Default entry form displayed at reload when Pool name contains spaces

--- portal/src/main/java/org/bonitasoft/console/client/user/cases/view/DisplayCaseFormPage.java
+++ portal/src/main/java/org/bonitasoft/console/client/user/cases/view/DisplayCaseFormPage.java
@@ -8,7 +8,7 @@
  *******************************************************************************/
 package org.bonitasoft.console.client.user.cases.view;
 
-import static org.bonitasoft.web.toolkit.client.common.i18n.AbstractI18n.*;
+import static org.bonitasoft.web.toolkit.client.common.i18n.AbstractI18n._;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -81,16 +81,16 @@ public class DisplayCaseFormPage extends Page {
     }
 
     private String getCaseOverviewUrl() {
-        final String processName = URL.encodeQueryString(this.getParameter(ProcessItem.ATTRIBUTE_NAME));
-        final String decodedProcessName = URL.encodeQueryString(processName);
-        final String processVersion = URL.encodeQueryString(this.getParameter(ProcessItem.ATTRIBUTE_VERSION));
+        final String encodedProcessName = this.getParameter(ProcessItem.ATTRIBUTE_NAME);
+        final String processName = URL.decodeQueryString(encodedProcessName);
+        final String encodedProcessVersion = this.getParameter(ProcessItem.ATTRIBUTE_VERSION);
         String caseId = this.getParameter(ArchivedCaseItem.ATTRIBUTE_SOURCE_OBJECT_ID);
         if (caseId == null) {
             caseId = this.getParameter(CaseItem.ATTRIBUTE_ID);
         }
         final String locale = AbstractI18n.getDefaultLocale().toString();
 
-        this.setTitle(_("Display a case form of process %app_name%", new Arg("app_name", decodedProcessName)));
+        this.setTitle(_("Display a case form of process %app_name%", new Arg("app_name", processName)));
 
         final StringBuilder frameURL = new StringBuilder();
         frameURL.append(GWT.getModuleBaseURL())
@@ -104,9 +104,9 @@ public class DisplayCaseFormPage extends Page {
         }
 
         frameURL.append("#form=")
-        .append(processName)
+                .append(encodedProcessName)
         .append(UUID_SEPERATOR)
-        .append(processVersion)
+                .append(encodedProcessVersion)
         .append("$recap&mode=form&instance=")
         .append(caseId).append("&recap=true");
         return frameURL.toString();
@@ -117,8 +117,8 @@ public class DisplayCaseFormPage extends Page {
             throw new RuntimeException(CaseItem.ATTRIBUTE_PROCESS_ID + " attribute need to be deployed");
         }
         final Map<String, String> processParams = new HashMap<String, String>();
-        processParams.put(ProcessItem.ATTRIBUTE_NAME, item.getProcess().getName());
-        processParams.put(ProcessItem.ATTRIBUTE_VERSION, item.getProcess().getVersion());
+        processParams.put(ProcessItem.ATTRIBUTE_NAME, URL.encodeQueryString(item.getProcess().getName()));
+        processParams.put(ProcessItem.ATTRIBUTE_VERSION, URL.encodeQueryString(item.getProcess().getVersion()));
         processParams.put(CaseItem.ATTRIBUTE_ID, item.getId().toString());
         if (item instanceof ArchivedCaseItem) {
             processParams.put(ArchivedCaseItem.ATTRIBUTE_SOURCE_OBJECT_ID, ((ArchivedCaseItem) item).getSourceObjectId().toString());
