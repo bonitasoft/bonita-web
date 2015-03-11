a3690be8e1f6e56a04e69cffa18f39d1acecad7c
anthony <anthony.birembaut@bonitasoft.com>
2015-03-11 13:59:52 +0100

    BS-12352 - Fix Homepage servlet in case there is no forms.xml

--- forms/forms-server/src/main/java/org/bonitasoft/forms/server/HomepageServlet.java
+++ forms/forms-server/src/main/java/org/bonitasoft/forms/server/HomepageServlet.java
@@ -15,6 +15,7 @@
 package org.bonitasoft.forms.server;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Date;
@@ -34,8 +35,10 @@ import org.bonitasoft.console.common.server.login.LoginManager;
 import org.bonitasoft.console.common.server.themes.CompilableFile;
 import org.bonitasoft.console.common.server.themes.ThemeArchive;
 import org.bonitasoft.console.common.server.themes.ThemeResourceServlet;
+import org.bonitasoft.console.common.server.utils.BPMEngineException;
 import org.bonitasoft.engine.api.TenantAPIAccessor;
 import org.bonitasoft.engine.api.ThemeAPI;
+import org.bonitasoft.engine.bpm.process.ProcessDefinitionNotFoundException;
 import org.bonitasoft.engine.exception.BonitaHomeNotSetException;
 import org.bonitasoft.engine.exception.ServerAPIException;
 import org.bonitasoft.engine.exception.UnknownAPITypeException;
@@ -43,6 +46,7 @@ import org.bonitasoft.engine.session.APISession;
 import org.bonitasoft.engine.theme.Theme;
 import org.bonitasoft.engine.theme.ThemeType;
 import org.bonitasoft.forms.server.accessor.impl.util.FormDocumentBuilderFactory;
+import org.bonitasoft.forms.server.exception.InvalidFormDefinitionException;
 import org.bonitasoft.forms.server.provider.FormServiceProvider;
 import org.bonitasoft.forms.server.provider.impl.util.FormServiceProviderFactory;
 import org.bonitasoft.forms.server.provider.impl.util.FormServiceProviderUtil;
@@ -204,7 +208,7 @@ public class HomepageServlet extends ThemeResourceServlet {
             final FormServiceProvider formServiceProvider = FormServiceProviderFactory.getFormServiceProvider(apiSession.getTenantId());
             final Date deployemenDate = formServiceProvider.getDeployementDate(context);
             // Make sure the application content has already been retrieved
-            FormDocumentBuilderFactory.getFormDocumentBuilder(apiSession, applicationID, Locale.ENGLISH.getLanguage(), deployemenDate);
+            ensureApplicationFolderExists(apiSession, applicationID, deployemenDate);
             myApplicationsFolder = formServiceProvider.getApplicationResourceDir(deployemenDate, context);
         } catch (final Throwable e) {
             final String errorMessage = "Error while using the servlet ThemeResourceServlet to get themes parent folder.";
@@ -214,6 +218,15 @@ public class HomepageServlet extends ThemeResourceServlet {
             throw new ServletException(errorMessage);
         }
         return myApplicationsFolder;
+    }
+
+    protected static void ensureApplicationFolderExists(final APISession apiSession, final long applicationID, final Date deployemenDate)
+            throws ProcessDefinitionNotFoundException, IOException, InvalidFormDefinitionException, BPMEngineException {
+        try {
+            FormDocumentBuilderFactory.getFormDocumentBuilder(apiSession, applicationID, Locale.ENGLISH.getLanguage(), deployemenDate);
+        } catch (final FileNotFoundException e) {
+            //Do nothing: there might be no forms.xml in the business archive
+        }
     }
 
     protected static APISession getEngineSession(final HttpServletRequest request) {
