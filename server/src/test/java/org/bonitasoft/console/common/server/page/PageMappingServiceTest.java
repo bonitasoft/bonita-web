242a07ac01829f0d94e9129d2af0121c1572b094
anthony <anthony.birembaut@bonitasoft.com>
2015-04-03 11:20:41 +0200

    BS-12902 - test for page mapping service

---
+++ server/src/test/java/org/bonitasoft/console/common/server/page/PageMappingServiceTest.java
@@ -0,0 +1,73 @@
+package org.bonitasoft.console.common.server.page;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.bonitasoft.console.common.server.login.LoginManager;
+import org.bonitasoft.engine.api.ProcessConfigurationAPI;
+import org.bonitasoft.engine.page.PageURL;
+import org.bonitasoft.engine.session.APISession;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PageMappingServiceTest {
+
+    @Spy
+    PageMappingService pageMappingService;
+
+    @Mock
+    ProcessConfigurationAPI processConfigurationAPI;
+
+    @Mock
+    HttpServletRequest hsRequest;
+
+    @Mock
+    HttpSession httpSession;
+
+    @Mock
+    APISession apiSession;
+
+    @Before
+    public void beforeEach() throws Exception {
+        when(hsRequest.getSession()).thenReturn(httpSession);
+        doReturn(processConfigurationAPI).when(pageMappingService).getProcessConfigurationAPI(apiSession);
+        when(apiSession.getUserId()).thenReturn(1L);
+    }
+
+    @Test
+    public void getPage() throws Exception {
+        final PageURL pageURL = mock(PageURL.class);
+        when(pageURL.getUrl()).thenReturn("/externalURL");
+        when(pageURL.getPageId()).thenReturn(null);
+        final Map<String, Serializable> context = new HashMap<String, Serializable>();
+        context.put("QUERY_PARAMETERS", (Serializable) hsRequest.getParameterMap());
+        context.put("IS_ADMIN", false);
+        when(processConfigurationAPI.resolvePageOrURL("process/processName/processVersion", context)).thenReturn(pageURL);
+        final Set<String> userPermissions = new HashSet<String>();
+        when(httpSession.getAttribute(LoginManager.PERMISSIONS_SESSION_PARAM_KEY)).thenReturn(userPermissions);
+
+        final PageReference returnedPageReference = pageMappingService.getPage(hsRequest, apiSession, "process/processName/processVersion");
+
+        assertNotNull(returnedPageReference);
+        assertEquals(null, returnedPageReference.getPageId());
+        assertEquals("/externalURL", returnedPageReference.getURL());
+    }
+
+}
