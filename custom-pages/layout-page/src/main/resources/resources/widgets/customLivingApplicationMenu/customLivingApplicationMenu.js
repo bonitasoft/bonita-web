9aa424a098cc9d8b4bad29e77d20e0cbc158a607
julien mege <julien.mege@bonitasoft.com>
2015-04-30 16:46:19 +0200

    BS-13244 Build the layout page using the last UI designer

--- custom-pages/layout-page/src/main/resources/resources/widgets/customLivingApplicationMenu/customLivingApplicationMenu.js
+++ custom-pages/layout-page/src/main/resources/resources/widgets/customLivingApplicationMenu/customLivingApplicationMenu.js
@@ -2,18 +2,35 @@ angular.module('org.bonitasoft.pagebuilder.widgets')
   .directive('customLivingApplicationMenu', function() {
     return {
       controllerAs: 'ctrl',
-      controller: function($scope, $location) {
-
-    this.selectTargetedMenu = function(menu) {
-        $scope.parameters.selectedMenu = menu;
-    };
+      controller: function WidgetlivingApplicationMenuController($scope, $http, $window) {
+    var ctrl = this;
     
-    this.hasChildren= function(menu, menuList) {
-        var children = menuList.filter(function(currentMenu){return currentMenu.parentMenuId===menu.id});
-        return children.length > 0;
+    var pathArray = $window.location.pathname.split( '/' ); 
+    ctrl.applicationToken =  pathArray[pathArray.length-2]; 
+    ctrl.pageToken =  pathArray[pathArray.length-1];
+
+    function getApplication() {
+        return $http.get('../../API/living/application?c=1&f=token%3D'+ctrl.applicationToken);
+    }
+   
+    function getApplicationMenuList(applicationId) {
+        $http.get('../../API/living/application-menu?c=100&f=applicationId%3D'+applicationId+'&d=applicationPageId&o=menuIndex+ASC')
+            .success(function(data) { 
+                ctrl.applicationMenuList = data;
+            });
     }
 
+    ctrl.hasChildren= function(menu) {
+        var children = ctrl.applicationMenuList.filter(function(currentMenu){return currentMenu.parentMenuId===menu.id});
+        return children.length > 0;
+    };
+    
+    getApplication().then(function(response) {
+        var application = response.data[0];
+        ctrl.applicationName = application.displayName;
+        return application.id;
+    }).then(getApplicationMenuList);
 },
-      template: '\n <nav class="navbar-inverse navbar" role="navigation>\n    <div class="navbar-header">\n      <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#application-menu">\n        <span class="sr-only">Toggle navigation</span>\n        <span class="icon-bar"></span>\n        <span class="icon-bar"></span>\n        <span class="icon-bar"></span>\n      </button>\n    </div>\n    <div class="collapse navbar-collapse" id="application-menu">\n      <ul class="nav navbar-nav">\n        <li dropdown="" ng-class="{active:parameters.selectedPageToken===menu.applicationPageId.token} {dropdown:ctrl.hasChildren(menu, parameters.menuList)===true} {open:ctrl.hasChildren(menu, parameters.menuList)===true}" ng-repeat="menu in parameters.menuList | filter:{parentMenuId:-1}">\n			<a ng-show="!ctrl.hasChildren(menu, parameters.menuList)" href="{{menu.applicationPageId.token}}" >{{menu.displayName}}</a>\n				\n            <a ng-show="ctrl.hasChildren(menu, parameters.menuList)" role="button" class="dropdown-toggle" dropdown-toggle="" aria-haspopup="true" aria-expanded="true">\n                {{menu.displayName}}<span class="caret"></span>\n            </a>\n    		<ul ng-show="ctrl.hasChildren(menu, parameters.menuList)" class="dropdown-menu">\n			    <li ng-repeat="childrenMenu in parameters.menuList | filter:{parentMenuId:menu.id}"><a href="{{childrenMenu.applicationPageId.token}}">{{childrenMenu.displayName}}</a>\n			    </li>\n		    </ul>\n        </li>\n      </ul>\n    </div>\n  </nav>'
+      template: '<div class="navbar navbar-inverse" role="navigation">\n      <div class="container">\n         <div class="container">  \n			<div class="navbar-header">\n				<a class="navbar-brand">{{ctrl.applicationName}}</a>\n		        <button type="button" ng-init="navCollapsed = true" ng-click="navCollapsed = !navCollapsed" class="navbar-toggle">\n                   <span class="icon-bar"></span>\n                   <span class="icon-bar"></span>\n                   <span class="icon-bar"></span>\n                </button>\n			</div>\n			<div collapse="navCollapsed" class="collapse navbar-responsive-collapse navbar-collapse">\n		      <ul class="nav navbar-nav">\n				<li ng-class="{active:ctrl.pageToken===menu.applicationPageId.token}" ng-repeat="menu in ctrl.applicationMenuList | filter:{parentMenuId:-1}" dropdown>\n					<a ng-if="!ctrl.hasChildren(menu)" href="{{menu.applicationPageId.token}}" >{{menu.displayName}}</a>            \n				    <a ng-if="ctrl.hasChildren(menu)" dropdown-toggle>{{menu.displayName}}<span class="caret"></span></a>\n				    <ul ng-if="ctrl.hasChildren(menu)" class="dropdown-menu">\n						<li ng-repeat="childrenMenu in ctrl.applicationMenuList | filter:{parentMenuId:menu.id}">\n							<a href="{{childrenMenu.applicationPageId.token}}">{{childrenMenu.displayName}}</a>\n						</li>\n				    </ul>\n				</li>\n			   </ul>\n		    </div>\n	    </div>\n    </div>\n</div>\n'
     };
   });
