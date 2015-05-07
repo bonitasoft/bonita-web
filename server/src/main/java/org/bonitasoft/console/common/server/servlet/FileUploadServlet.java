f5dea5095b9955786d320b1cb8ae6585fd4c3646
anthony <anthony.birembaut@bonitasoft.com>
2015-05-07 11:14:15 +0200

    BS-13010 - rename fileName into filename for File contract inputs

--- server/src/main/java/org/bonitasoft/console/common/server/servlet/FileUploadServlet.java
+++ server/src/main/java/org/bonitasoft/console/common/server/servlet/FileUploadServlet.java
@@ -68,7 +68,7 @@ public abstract class FileUploadServlet extends HttpServlet {
 
     protected static final String TEMP_PATH_RESPONSE_ATTRIBUTE = "tempPath";
 
-    protected static final String FILE_NAME_RESPONSE_ATTRIBUTE = "fileName";
+    protected static final String FILE_NAME_RESPONSE_ATTRIBUTE = "filename";
 
     protected String[] supportedExtensionsList = new String[0];
 
