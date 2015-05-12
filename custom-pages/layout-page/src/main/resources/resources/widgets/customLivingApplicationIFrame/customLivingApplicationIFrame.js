9fdd2efb4d5c820748ef79edc5ee00b375ffbd5b
julien mege <julien.mege@bonitasoft.com>
2015-05-12 17:16:01 +0200

    BS-12836 Update Layout page

--- custom-pages/layout-page/src/main/resources/resources/widgets/customLivingApplicationIFrame/customLivingApplicationIFrame.js
+++ custom-pages/layout-page/src/main/resources/resources/widgets/customLivingApplicationIFrame/customLivingApplicationIFrame.js
@@ -2,35 +2,7 @@ angular.module('org.bonitasoft.pagebuilder.widgets')
   .directive('customLivingApplicationIFrame', function() {
     return {
       controllerAs: 'ctrl',
-      controller: function WidgetlivingApplicationIFrameController($scope, $http, $window) {
-    var ctrl = this;
-    
-    var pathArray = $window.location.pathname.split( '/' ); 
-    ctrl.applicationToken =  pathArray[pathArray.length-2]; 
-    ctrl.pageToken =  pathArray[pathArray.length-1];
-    ctrl.queryString = $window.location.search.substring(1);
-    
-    function getApplication() {
-        return $http.get('../../API/living/application?c=1&f=token%3D'+ctrl.applicationToken);
-    }
-   
-    function getTargetedApplicationPage(applicationId) {
-        $http.get('../../API/living/application-page?c=1&f=token%3D'+ctrl.pageToken+'&f=applicationId%3D'+applicationId+'&d=pageId')
-            .success(function(data) { 
-                ctrl.customPageToken = data[0].pageId.urlToken;
-            });
-    }
-
-    getApplication().then(function(response) {
-        var application = response.data[0];
-        ctrl.applicationId = application.id;
-        return application.id;
-    }).then(getTargetedApplicationPage);
-     
-    ctrl.getSrc = function(){
-        return '/bonita/portal/custom-page/'+ctrl.customPageToken+'/?appToken='+ctrl.applicationToken+'&'+ctrl.queryString;
-    };
-    
+      controller: function WidgetlivingApplicationIFrameController($scope) {
 },
       template: '\n<iframe id="livingAppIFrame" width="100%" height="{{properties.contentHeight}}" style="border: 0" ng-src="{{properties.src}}"></iframe>'
     };
--- custom-pages/layout-page/src/main/resources/resources/widgets/customLivingApplicationMenu/customLivingApplicationMenu.js
+++ custom-pages/layout-page/src/main/resources/resources/widgets/customLivingApplicationMenu/customLivingApplicationMenu.js
@@ -6,41 +6,37 @@ angular.module('org.bonitasoft.pagebuilder.widgets')
     var ctrl = this;
     
     var pathArray = $window.location.pathname.split( '/' ); 
-    ctrl.applicationToken =  pathArray[pathArray.length-2]; 
-    ctrl.pageToken =  pathArray[pathArray.length-1];
+    ctrl.applicationToken =  pathArray[pathArray.length-3]; 
+    ctrl.pageToken =  pathArray[pathArray.length-2];
     
 
     function getApplication() {
-        return $http.get('../../API/living/application?c=1&f=token%3D'+ctrl.applicationToken);
+        return $http.get('../API/living/application/?c=1&f=token%3D'+ctrl.applicationToken);
     }
    
     function getApplicationMenuList(applicationId) {
         
-        $http.get('../../API/living/application-menu?c=100&f=applicationId%3D'+applicationId+'&d=applicationPageId&o=menuIndex+ASC')
+        $http.get('../API/living/application-menu/?c=100&f=applicationId%3D'+applicationId+'&d=applicationPageId&o=menuIndex+ASC')
             .success(function(data) { 
                 ctrl.applicationMenuList = data;
             });
         return applicationId;
     }
 
-    function getTargetedApplicationPage(applicationId) {
-        $http.get('../../API/living/application-page?c=1&f=token%3D'+ctrl.pageToken+'&f=applicationId%3D'+applicationId+'&d=pageId')
-            .success(function(data) { 
-                $scope.properties.targetUrl = $scope.properties.contextPath+"/portal/resource/app/"+ctrl.applicationToken+"/"+data[0].pageId.urlToken+"/content/"+ $window.location.search;
-            });
+    function setTargetedUrl() {
+        $scope.properties.targetUrl = $scope.properties.contextPath+"/portal/resource/app/"+ctrl.applicationToken+"/"+ ctrl.pageToken+"/content/"+ $window.location.search;
     }
 
-    ctrl.hasChildren= function(menu) {
-        var children = ctrl.applicationMenuList.filter(function(currentMenu){return currentMenu.parentMenuId===menu.id});
-        return children.length > 0;
+    ctrl.isParentMenu= function(menu) {
+        return menu.parentMenuId==-1 && menu.applicationPageId==-1;
     };
     
     getApplication().then(function(response) {
         var application = response.data[0];
         ctrl.applicationName = application.displayName;
         return application.id;
-    }).then(getApplicationMenuList).then(getTargetedApplicationPage);
+    }).then(getApplicationMenuList).then(setTargetedUrl);
 },
-      template: '<div class="navbar navbar-inverse" role="navigation">\n      <div class="container">\n         <div class="container">  \n			<div class="navbar-header">\n				<a class="navbar-brand">{{ctrl.applicationName}}</a>\n		        <button type="button" ng-init="navCollapsed = true" ng-click="navCollapsed = !navCollapsed" class="navbar-toggle">\n                   <span class="icon-bar"></span>\n                   <span class="icon-bar"></span>\n                   <span class="icon-bar"></span>\n                </button>\n			</div>\n			<div collapse="navCollapsed" class="collapse navbar-responsive-collapse navbar-collapse">\n		      <ul class="nav navbar-nav">\n				<li ng-class="{active:ctrl.pageToken===menu.applicationPageId.token}" ng-repeat="menu in ctrl.applicationMenuList | filter:{parentMenuId:-1}" dropdown>\n					<a ng-if="!ctrl.hasChildren(menu)" href="{{menu.applicationPageId.token}}" >{{menu.displayName}}</a>            \n				    <a ng-if="ctrl.hasChildren(menu)" dropdown-toggle>{{menu.displayName}}<span class="caret"></span></a>\n				    <ul ng-if="ctrl.hasChildren(menu)" class="dropdown-menu">\n						<li ng-repeat="childrenMenu in ctrl.applicationMenuList | filter:{parentMenuId:menu.id}">\n							<a href="{{childrenMenu.applicationPageId.token}}">{{childrenMenu.displayName}}</a>\n						</li>\n				    </ul>\n				</li>\n			   </ul>\n		    </div>\n	    </div>\n    </div>\n</div>\n'
+      template: '<div class="navbar navbar-inverse" role="navigation">\n      <div class="container">\n         <div class="container">  \n			<div class="navbar-header">\n				<a class="navbar-brand">{{ctrl.applicationName}}</a>\n		        <button type="button" ng-init="navCollapsed = true" ng-click="navCollapsed = !navCollapsed" class="navbar-toggle">\n                   <span class="icon-bar"></span>\n                   <span class="icon-bar"></span>\n                   <span class="icon-bar"></span>\n                </button>\n			</div>\n			\n			<div collapse="navCollapsed" class="collapse navbar-responsive-collapse navbar-collapse">\n		      <ul class="nav navbar-nav">\n				<li ng-class="{active:ctrl.pageToken===menu.applicationPageId.token}" ng-repeat="menu in ctrl.applicationMenuList | filter:{parentMenuId:-1}" dropdown>\n					<a ng-if="!ctrl.isParentMenu(menu)" ng-href="../{{menu.applicationPageId.token}}/" >{{menu.displayName}}</a>            \n				    <a ng-if="ctrl.isParentMenu(menu)" dropdown-toggle>{{menu.displayName}}<span class="caret"></span></a>\n				    <ul ng-if="ctrl.isParentMenu(menu)" class="dropdown-menu">\n						<li ng-repeat="childMenu in ctrl.applicationMenuList | filter:{parentMenuId:menu.id}">\n							<a ng-href="../{{childMenu.applicationPageId.token}}/">{{childMenu.displayName}}</a>\n						</li>\n				    </ul>\n				</li>\n			   </ul>\n		    </div>\n	    </div>\n    </div>\n</div>\n'
     };
   });
