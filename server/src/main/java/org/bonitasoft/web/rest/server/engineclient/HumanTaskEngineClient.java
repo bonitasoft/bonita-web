81db364b0e8810880dd0d48f3392ea7ac3f36e34
anthony <anthony.birembaut@bonitasoft.com>
2016-01-05 11:04:12 +0100

    BS-14492 - add original exception to APIExceptions

--- server/src/main/java/org/bonitasoft/web/rest/server/engineclient/HumanTaskEngineClient.java
+++ server/src/main/java/org/bonitasoft/web/rest/server/engineclient/HumanTaskEngineClient.java
@@ -5,20 +5,25 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.web.rest.server.engineclient;
 
 
+import java.util.List;
+
 import org.bonitasoft.engine.api.ProcessAPI;
-import org.bonitasoft.engine.bpm.flownode.*;
+import org.bonitasoft.engine.bpm.flownode.ActivityInstanceNotFoundException;
+import org.bonitasoft.engine.bpm.flownode.ArchivedHumanTaskInstance;
+import org.bonitasoft.engine.bpm.flownode.HumanTaskInstance;
+import org.bonitasoft.engine.bpm.flownode.HumanTaskInstanceSearchDescriptor;
 import org.bonitasoft.engine.exception.SearchException;
 import org.bonitasoft.engine.search.SearchOptions;
 import org.bonitasoft.engine.search.SearchOptionsBuilder;
@@ -28,45 +33,43 @@ import org.bonitasoft.web.toolkit.client.common.exception.api.APIException;
 import org.bonitasoft.web.toolkit.client.common.exception.api.APIItemNotFoundException;
 import org.bonitasoft.web.toolkit.client.data.APIID;
 
-import java.util.List;
-
 /**
  * @author Colin PUY
  * @author Elias Ricken de Medeiros
- * 
+ *
  */
 public class HumanTaskEngineClient {
 
-    private ProcessAPI processAPI;
+    private final ProcessAPI processAPI;
 
-    public HumanTaskEngineClient(ProcessAPI processAPI) {
+    public HumanTaskEngineClient(final ProcessAPI processAPI) {
         this.processAPI = processAPI;
     }
 
     public long countOpenedHumanTasks() {
-        SearchOptions search = new SearchOptionsBuilder(0, 0)
+        final SearchOptions search = new SearchOptionsBuilder(0, 0)
                 .filter(HumanTaskInstanceSearchDescriptor.STATE_NAME, HumanTaskItem.VALUE_STATE_READY).done();
         try {
             return processAPI.searchHumanTaskInstances(search).getCount();
-        } catch (Exception e) {
-            throw new APIException("Error when counting opened cases");
+        } catch (final Exception e) {
+            throw new APIException("Error when counting opened cases", e);
         }
     }
-    
-    public List<ArchivedHumanTaskInstance> searchArchivedHumanTasks(SearchOptions searchOptions) {
+
+    public List<ArchivedHumanTaskInstance> searchArchivedHumanTasks(final SearchOptions searchOptions) {
         try {
             return processAPI.searchArchivedHumanTasks(searchOptions).getResult();
-        } catch (SearchException e) {
+        } catch (final SearchException e) {
             throw new APIException("Error when searching archived human tasks", e);
         }
     }
 
-    public HumanTaskInstance getHumanTaskInstance(long humanTaskInstanceId) {
+    public HumanTaskInstance getHumanTaskInstance(final long humanTaskInstanceId) {
         try {
             return processAPI.getHumanTaskInstance(humanTaskInstanceId);
-        } catch (ActivityInstanceNotFoundException e) {
+        } catch (final ActivityInstanceNotFoundException e) {
             throw new APIItemNotFoundException(HumanTaskDefinition.TOKEN, APIID.makeAPIID(humanTaskInstanceId));
-        } catch (Exception e) {
+        } catch (final Exception e) {
             throw new APIException("Error when getting human task instance", e);
         }
     }
