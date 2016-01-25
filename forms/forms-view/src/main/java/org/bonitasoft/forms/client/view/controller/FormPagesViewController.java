9af386d4bebb5ed91d5974325576bc916cac4c37
anthony <anthony.birembaut@bonitasoft.com>
2016-01-25 11:36:31 +0100

    BS-14888 - Confirmation page V6 referencing field fails retrieving Form field in a groovy script

--- forms/forms-view/src/main/java/org/bonitasoft/forms/client/view/controller/FormPagesViewController.java
+++ forms/forms-view/src/main/java/org/bonitasoft/forms/client/view/controller/FormPagesViewController.java
@@ -1390,7 +1390,7 @@ public class FormPagesViewController {
         } else {
             defaultConfirmationMessage = FormsResourceBundle.getMessages().submissionConfirmationMessage();
         }
-        formsServiceAsync.getFormConfirmationTemplate(formID, urlContext, createConfirmationPageHandler(defaultConfirmationMessage));
+        formsServiceAsync.getFormConfirmationTemplate(formID, urlContext, widgetValues, createConfirmationPageHandler(defaultConfirmationMessage));
     }
 
     private ConfirmationPageHandler createConfirmationPageHandler(final String defaultConfirmationMessage) {
