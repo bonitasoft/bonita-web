7f04d9aff6ea5cb305f838f709e12317c669d6f0
julienmege <julien.mege@bonitasoft.com>
2016-09-28 10:13:04 +0200

    fix(csrf) Do not check CSRFToken on GET (#131)
    Will allows Avatars to be shown in the portal
    
    Closes [BS-15824](https://bonitasoft.atlassian.net/browse/BS-15824_)

--- server/src/main/java/org/bonitasoft/console/common/server/login/filter/TokenValidatorFilter.java
+++ server/src/main/java/org/bonitasoft/console/common/server/login/filter/TokenValidatorFilter.java
@@ -5,43 +5,37 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.console.common.server.login.filter;
 
-import java.util.logging.Level;
+import org.bonitasoft.console.common.server.preferences.properties.PropertiesFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.lang3.StringUtils;
-import org.bonitasoft.console.common.server.preferences.properties.PropertiesFactory;
+import java.util.logging.Level;
 
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
-
 /**
  * @author Paul AMAR
- *
  */
 public class TokenValidatorFilter extends AbstractAuthorizationFilter {
 
     @Override
     boolean checkValidCondition(HttpServletRequest httpRequest, HttpServletResponse httpResponse) {
-        if (isCsrfProtectionEnabled()) {
+        if (isCsrfProtectionEnabled() && !isGetMethod(httpRequest.getMethod())) {
             String headerFromRequest = getCSRFToken(httpRequest);
             String apiToken = (String) httpRequest.getSession().getAttribute("api_token");
 
             if (headerFromRequest == null || !headerFromRequest.equals(apiToken)) {
                 if (LOGGER.isLoggable(Level.FINE)) {
-                        LOGGER.log(Level.FINE, "Token Validation failed, expected: " + apiToken + ", received: " + headerFromRequest);    
+                    LOGGER.log(Level.FINE, "Token Validation failed, expected: " + apiToken + ", received: " + headerFromRequest);
                 }
                 httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                 return false;
@@ -63,5 +57,8 @@ public class TokenValidatorFilter extends AbstractAuthorizationFilter {
         return token;
     }
 
+    private boolean isGetMethod(String method) {
+        return "GET".equalsIgnoreCase(method);
+    }
 
 }
