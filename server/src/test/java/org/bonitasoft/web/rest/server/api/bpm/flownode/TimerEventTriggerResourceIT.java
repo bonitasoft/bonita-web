fc716d33197e1d81423265d57fd64d828db4ec6a
abirembaut <anthony.birembaut@gmail.com>
2017-03-01 16:59:19 +0100

    TR(restlet) upgrade restlet version to 2.3.9 (#187)
    * Revert "Feat/wildfly-restlet-patch (#108)"
    
    This reverts commit ed2c1d9f43ca2de21d4ca87a94818f526b70fb8f.
    
    * chore(restlet) upgrade restlet version to 2.3.9
    
    - including restlet/restlet-framework-java/pull/1229 to avoid patching restlet
    - follow restlet changes (preventing to set the Content-range as we want)

--- server/src/test/java/org/bonitasoft/web/rest/server/api/bpm/flownode/TimerEventTriggerResourceIT.java
+++ server/src/test/java/org/bonitasoft/web/rest/server/api/bpm/flownode/TimerEventTriggerResourceIT.java
@@ -23,7 +23,7 @@ import org.bonitasoft.test.toolkit.organization.TestUserFactory;
 import org.bonitasoft.web.rest.server.AbstractConsoleTest;
 import org.junit.Test;
 import org.restlet.Response;
-import org.restlet.util.Series;
+import org.restlet.representation.Representation;
 
 public class TimerEventTriggerResourceIT extends AbstractConsoleTest {
 
@@ -36,24 +36,23 @@ public class TimerEventTriggerResourceIT extends AbstractConsoleTest {
         final HttpSession session = mock(HttpSession.class);
         doReturn(session).when(restResource).getHttpSession();
         final APISession apiSession = TestUserFactory.getJohnCarpenter().getSession();
-        //new APISessionImpl(14L, new Date(), 3000000L, "username", 1L, "default", 1L);
         doReturn(apiSession).when(session).getAttribute("apiSession");
         doReturn(0).when(restResource).getIntegerParameter(anyString(), anyBoolean());
         doReturn(0L).when(restResource).getLongParameter(anyString(), anyBoolean());
         doReturn("").when(restResource).getParameter(anyString(), anyBoolean());
         final Response response = mock(Response.class);
-        doReturn(mock(Series.class)).when(response).getHeaders();
+        doReturn(mock(Representation.class)).when(response).getEntity();
         doReturn(response).when(restResource).getResponse();
         doReturn(Collections.emptyList()).when(restResource).getParameterAsList(anyString());
     }
 
     @Test
-    public void searchTimerEventTriggersShouldReturnStatusCode200() throws Exception {
-        assertThat(restResource.searchTimerEventTriggers().toString()).isEqualTo("[]");
+    public void should_search_timer_event_triggers() throws Exception {
+        restResource.searchTimerEventTriggers();
     }
 
     @Test(expected = TimerEventTriggerInstanceNotFoundException.class)
-    public void updateTimerEventTriggersShouldThrowExceptionIfTimerNotFound() throws Exception {
+    public void should_throw_exception_when_timer_event_triggers_not_found() throws Exception {
         doReturn("1").when(restResource).getAttribute(TimerEventTriggerResource.ID_PARAM_NAME);
 
         final TimerEventTrigger trigger = new TimerEventTrigger(System.currentTimeMillis());
@@ -61,7 +60,7 @@ public class TimerEventTriggerResourceIT extends AbstractConsoleTest {
     }
 
     @Test
-    public void updateTimerEventTriggersShouldReturnStatusCode200() throws Exception {
+    public void should_update_timer_event_triggers() throws Exception {
         final long timerEventTriggerId = 1L;
         final ProcessAPI processAPI = mock(ProcessAPI.class);
         restResource = spy(new TimerEventTriggerResource(processAPI));
