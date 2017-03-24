fd85dab2d7c25bf628962014109cbb4d68813bca
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2017-03-24 09:36:15 +0100

    feat(lib): upgrade httpClient to 4.5.2 (#200)
    * lib was used but not declared as dependency
    * transitive dependency httpcore is also required until engine updates also to httpClient 4.5.2
    
    Part of [BS-16290](https://bonitasoft.atlassian.net/browse/BS-16290)

--- server/src/main/java/org/bonitasoft/console/common/server/login/localization/UrlBuilder.java
+++ server/src/main/java/org/bonitasoft/console/common/server/login/localization/UrlBuilder.java
@@ -5,12 +5,10 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -51,7 +49,10 @@ public class UrlBuilder {
                 .setPort(uri.getPort())
                 .setPath(uri.getPath())
                 .setFragment(uri.getFragment());
-        parameters.addAll(URLEncodedUtils.parse(uri.getQuery(), Charset.forName("UTF8")));
+        if (uri.getQuery() != null) {
+            //avoid NPE thrown by httpClient 4.5.2 regression
+            parameters.addAll(URLEncodedUtils.parse(uri.getQuery(), Charset.forName("UTF8")));
+        }
     }
 
     public void appendParameter(final String key, final String value) {
