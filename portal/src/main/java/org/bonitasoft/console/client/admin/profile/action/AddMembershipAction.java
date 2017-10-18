dc63f8b83fea0d09d3f63206ec7b21f00fe85567
Colin PUY <colin.puy@bonitasoft.com>
2017-10-18 14:39:56 +0200

    chore(organisation): remove dead code
    Remove all portal code related to admin user more details since it is now implemented in angular
    
    Relates to [BS-16010](https://bonitasoft.atlassian.net/browse/BS-16010)

--- portal/src/main/java/org/bonitasoft/console/client/admin/profile/action/AddMembershipAction.java
+++ portal/src/main/java/org/bonitasoft/console/client/admin/profile/action/AddMembershipAction.java
@@ -1,47 +1,49 @@
 /**
  * Copyright (C) 2011 BonitaSoft S.A.
- * BonitaSoft, 31 rue Gustave Eiffel - 38000 Grenoble
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.bonitasoft.console.client.admin.organization.users.action;
+package org.bonitasoft.console.client.admin.profile.action;
 
 import java.util.Map;
 
-import org.bonitasoft.web.rest.model.identity.MembershipDefinition;
-import org.bonitasoft.web.rest.model.identity.MembershipItem;
 import org.bonitasoft.web.toolkit.client.ViewController;
-import org.bonitasoft.web.toolkit.client.data.APIID;
 import org.bonitasoft.web.toolkit.client.data.api.APICaller;
 import org.bonitasoft.web.toolkit.client.data.api.callback.APICallback;
-import org.bonitasoft.web.toolkit.client.data.item.Definitions;
-import org.bonitasoft.web.toolkit.client.ui.action.Action;
+import org.bonitasoft.web.toolkit.client.data.item.ItemDefinition;
+import org.bonitasoft.web.toolkit.client.ui.action.form.FormAction;
 
 /**
  * @author Séverin Moussel
  */
-public class DeleteMembershipAction extends Action {
-
+public class AddMembershipAction extends FormAction {
+    
+    private ItemDefinition definition;
+    
+    public AddMembershipAction(ItemDefinition def) {
+        this.definition = def;
+    }
+    
     @Override
     public void execute() {
+        new APICaller(definition).add(getForm(), new APICallback() {
 
-        new APICaller<MembershipItem>(Definitions.get(MembershipDefinition.TOKEN)).delete(
-                APIID.makeAPIID(this.getParameter("id")),
-                new APICallback() {
+            @Override
+            public void onSuccess(final int httpStatusCode, final String response, final Map<String, String> headers) {
+                ViewController.getInstance().historyBack();
+            }
 
-                    @Override
-                    public void onSuccess(final int httpStatusCode, final String response, final Map<String, String> headers) {
-                        ViewController.getInstance().historyBack();
-                    }
-
-                });
+        });
     }
 }
