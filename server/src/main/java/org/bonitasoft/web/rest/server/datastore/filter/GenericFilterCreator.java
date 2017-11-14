768627720ede29666cb4559e19b1d54a03738425
Romain Bioteau <romain.bioteau@bonitasoft.com>
2017-11-14 13:37:00 +0100

    fix(search) handle hasNavigation filter on profiles (#318)
    * add support for `differentFrom` operator
    * convert `hasNavigation` filter into proper engine search options
    
    Closes [BS-17411](https://bonitasoft.atlassian.net/browse/BS-17411)

--- server/src/main/java/org/bonitasoft/web/rest/server/datastore/filter/GenericFilterCreator.java
+++ server/src/main/java/org/bonitasoft/web/rest/server/datastore/filter/GenericFilterCreator.java
@@ -5,12 +5,10 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -20,11 +18,10 @@ import org.bonitasoft.web.rest.server.datastore.converter.AttributeConverter;
 
 /**
  * @author Vincent Elcrin
- * 
  */
 public class GenericFilterCreator implements FilterCreator {
 
-    private AttributeConverter fieldConverter;
+    protected AttributeConverter fieldConverter;
 
     public GenericFilterCreator(AttributeConverter fieldConverter) {
         this.fieldConverter = fieldConverter;
