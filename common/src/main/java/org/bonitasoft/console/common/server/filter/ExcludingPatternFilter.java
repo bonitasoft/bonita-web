700e537d681c0bae6672423fd63c1e809b03252f
abirembaut <anthony.birembaut@gmail.com>
2018-09-10 15:26:20 +0200

    feat(Kerberos) Support Single Sign On with Kerberos (#414)
    - extract URL pattern exclusion code from filters
    - add Kerberos filter and remote user authentication manager
    
    Covers [BS-18717](https://bonitasoft.atlassian.net/browse/BS-18717)

--- common/src/main/java/org/bonitasoft/console/common/server/filter/ExcludingPatternFilter.java
+++ common/src/main/java/org/bonitasoft/console/common/server/filter/ExcludingPatternFilter.java
@@ -1,9 +1,6 @@
 package org.bonitasoft.console.common.server.filter;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
 import javax.servlet.Filter;
@@ -14,25 +11,17 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang3.StringUtils;
-
 
 /**
  * @author Anthony Birembaut
  */
 public abstract class ExcludingPatternFilter implements Filter {
 
-    /**
-     * Logger
-     */
-    private static final Logger LOGGER = Logger.getLogger(ExcludingPatternFilter.class.getName());
-    
-    /** the Pattern of url not to filter */
-    protected Pattern excludePattern = null;
+    protected URLExcludePattern urlExcludePattern;
     
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        initExcludePattern(filterConfig);
+        urlExcludePattern = new URLExcludePattern(filterConfig, getDefaultExcludedPages());
     }
     
     @Override
@@ -55,30 +44,10 @@ public abstract class ExcludingPatternFilter implements Filter {
 
     public abstract void proceedWithFiltering(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException;
 
-    protected void initExcludePattern(final FilterConfig filterConfig) {
-        final String contextPath = filterConfig.getServletContext().getContextPath();
-        String webappName;
-        if (contextPath.length() > 0) {
-            webappName = contextPath.substring(1);
-        } else {
-            webappName = "";
-        }
-        excludePattern = compilePattern(StringUtils.isBlank(filterConfig.getInitParameter("excludePattern")) ? getDefaultExcludedPages()
-                .replace("bonita", webappName)
-                : filterConfig.getInitParameter("excludePattern"));
-    }
-
     public abstract String getDefaultExcludedPages();
 
     public Pattern compilePattern(final String stringPattern) {
-        if (StringUtils.isNotBlank(stringPattern)) {
-            try {
-                return Pattern.compile(stringPattern);
-            } catch (final Exception e) {
-                LOGGER.log(Level.SEVERE, " impossible to create pattern from [" + stringPattern + "] : " + e);
-            }
-        }
-        return null;
+        return urlExcludePattern.compilePattern(stringPattern);
     }
 
     /**
@@ -89,27 +58,11 @@ public abstract class ExcludingPatternFilter implements Filter {
      * @return true if the url match the pattern
      */
     public boolean matchExcludePatterns(final String url) {
-        try {
-            final boolean isExcluded = getExcludePattern() != null && getExcludePattern().matcher(new URL(url).getPath()).find();
-            if (LOGGER.isLoggable(Level.FINE)) {
-                if (isExcluded) {
-                    LOGGER.log(Level.FINE, " Exclude pattern match with this url:" + url);
-                } else {
-                    LOGGER.log(Level.FINE, " Exclude pattern does not match with this url:" + url);
-                }
-            }
-            return isExcluded;
-        } catch (final Exception e) {
-            if (LOGGER.isLoggable(Level.INFO)) {
-                LOGGER.log(Level.INFO, "impossible to get URL from given input [" + url + "]:" + e);
-            }
-            return getExcludePattern().matcher(url).find();
-
-        }
+        return urlExcludePattern.matchExcludePatterns(url);
     }
 
     public Pattern getExcludePattern() {
-        return excludePattern;
+        return urlExcludePattern.getExcludePattern();
     }
     
 }
