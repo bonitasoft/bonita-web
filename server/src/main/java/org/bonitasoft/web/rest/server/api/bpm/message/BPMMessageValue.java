68344b77c7f6590a90fc6a069e1a5602912ee518
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-09-11 18:22:52 +0200

    fea(sendMessage) add type for message content and correlation values

---
+++ server/src/main/java/org/bonitasoft/web/rest/server/api/bpm/message/BPMMessageValue.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2018 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.web.rest.server.api.bpm.message;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+public class BPMMessageValue {
+
+    @JsonInclude(Include.NON_EMPTY)
+    private String type;
+    private Object value;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public static BPMMessageValue create(Object value, String type) {
+        BPMMessageValue res = new BPMMessageValue();
+        res.setValue(value);
+        res.setType(type);
+        return res;
+    }
+
+}
