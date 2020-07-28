2c8c4daa07d0e1bb69c6b8952b30b233aa2f2d26
adrien <adrien.lachambre@bonitasoft.com>
2020-07-28 10:29:03 +0200

    * Validate that className isn't empty * Add some JavaDoc for the class ControllerClassName

--- server/src/main/java/org/bonitasoft/web/rest/server/api/extension/ControllerClassName.java
+++ server/src/main/java/org/bonitasoft/web/rest/server/api/extension/ControllerClassName.java
@@ -14,6 +14,12 @@
  */
 package org.bonitasoft.web.rest.server.api.extension;
 
+/**
+ * This class has the responsability to store a REST API extension controller class name, and if this REST API is used in
+ * compiled mode or in source mode.
+ * - In compiled mode, name is supposed to be the qualified name of a Java class present in the Jar.
+ * - In source mode, name is supposed to be the path to the main Groovy source file
+ */
 public class ControllerClassName {
 
     private String name;
