93cf74868b64bba5ba690d77efbcca4d290ec0e5
abirembaut <anthony.birembaut@bonitasoft.com>
2021-04-08 09:49:17 +0200

    fix(HTTP headers): enforce X-Content-Type and X-Frame headers (#685)
    * fix(HTTP headers): enforce X-Content-Type and X-Frame headers
    
    - set headers only once (use setHeader instead of addHeader)
    
    Relates to [BPO-664](https://bonitasoft.atlassian.net/browse/BPO-664)

--- common/src/main/java/org/bonitasoft/console/common/server/login/filter/ContentTypeSecurityFilter.java
+++ common/src/main/java/org/bonitasoft/console/common/server/login/filter/ContentTypeSecurityFilter.java
@@ -5,12 +5,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -30,37 +30,38 @@ import org.apache.commons.lang3.StringUtils;
 import org.bonitasoft.console.common.server.filter.ExcludingPatternFilter;
 
 /**
+ * Security filter setting the X-Content-Type-Options in the response headers
+ * 
  * @author Paul AMAR
  * @author Anthony Birembaut
- *
  */
 public class ContentTypeSecurityFilter extends ExcludingPatternFilter {
 
-	protected static final String X_CONTENT_TYPE_HEADER = "X-Content-Type-Options";
+    protected static final String X_CONTENT_TYPE_HEADER = "X-Content-Type-Options";
 
-	protected String headerValue;
+    protected String headerValue;
 
-	@Override
-	public String getDefaultExcludedPages() {
-		return "";
-	}
+    @Override
+    public String getDefaultExcludedPages() {
+        return "";
+    }
 
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-		headerValue = StringUtils.defaultIfEmpty(filterConfig.getInitParameter(X_CONTENT_TYPE_HEADER), "nosniff");
-		super.init(filterConfig);
-	}
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        headerValue = StringUtils.defaultIfEmpty(filterConfig.getInitParameter(X_CONTENT_TYPE_HEADER), "nosniff");
+        super.init(filterConfig);
+    }
 
-	@Override
-	public void proceedWithFiltering(final ServletRequest request, final ServletResponse response,
-			final FilterChain chain) throws ServletException, IOException {
-		// casting to HTTPServlet(Request|Response)
-		final HttpServletRequest req = (HttpServletRequest) request;
-		final HttpServletResponse res = (HttpServletResponse) response;
+    @Override
+    public void proceedWithFiltering(final ServletRequest request, final ServletResponse response,
+            final FilterChain chain) throws ServletException, IOException {
+        // casting to HTTPServlet(Request|Response)
+        final HttpServletRequest req = (HttpServletRequest) request;
+        final HttpServletResponse res = (HttpServletResponse) response;
 
-		// X-Content-Type-Options (Drive-by download attacks)
-		res.addHeader(X_CONTENT_TYPE_HEADER, headerValue);
+        chain.doFilter(req, res);
 
-		chain.doFilter(req, res);
-	}
+        // X-Content-Type-Options (Drive-by download attacks)
+        res.setHeader(X_CONTENT_TYPE_HEADER, headerValue);
+    }
 }
--- common/src/main/java/org/bonitasoft/console/common/server/login/filter/FrameSecurityFilter.java
+++ common/src/main/java/org/bonitasoft/console/common/server/login/filter/FrameSecurityFilter.java
@@ -5,12 +5,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -30,38 +30,39 @@ import org.apache.commons.lang3.StringUtils;
 import org.bonitasoft.console.common.server.filter.ExcludingPatternFilter;
 
 /**
+ * Security filter setting the X-Frame-Options in the response headers
+ * 
  * @author Anthony Birembaut
- *
  */
 public class FrameSecurityFilter extends ExcludingPatternFilter {
 
-	protected static final String X_FRAME_OPTIONS_HEADER = "X-Frame-Options";
+    protected static final String X_FRAME_OPTIONS_HEADER = "X-Frame-Options";
 
-	protected static final String FRAME_FILTER_EXCLUDED_PAGES_PATTERN = "^/(bonita/)?(portal/resource/.+/content/$)|(portal/custom-page/.+/$)";
+    protected static final String FRAME_FILTER_EXCLUDED_PAGES_PATTERN = "^/(bonita/)?(portal/resource/.+/content/$)|(portal/custom-page/.+/$)";
 
-	protected String headerValue;
+    protected String headerValue;
 
-	@Override
-	public String getDefaultExcludedPages() {
-		return FRAME_FILTER_EXCLUDED_PAGES_PATTERN;
-	}
+    @Override
+    public String getDefaultExcludedPages() {
+        return FRAME_FILTER_EXCLUDED_PAGES_PATTERN;
+    }
 
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-		headerValue = StringUtils.defaultIfEmpty(filterConfig.getInitParameter(X_FRAME_OPTIONS_HEADER), "SAMEORIGIN");
-		super.init(filterConfig);
-	}
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        headerValue = StringUtils.defaultIfEmpty(filterConfig.getInitParameter(X_FRAME_OPTIONS_HEADER), "SAMEORIGIN");
+        super.init(filterConfig);
+    }
 
-	@Override
-	public void proceedWithFiltering(final ServletRequest request, final ServletResponse response,
-			final FilterChain chain) throws ServletException, IOException {
-		// casting to HTTPServlet(Request|Response)
-		final HttpServletRequest req = (HttpServletRequest) request;
-		final HttpServletResponse res = (HttpServletResponse) response;
+    @Override
+    public void proceedWithFiltering(final ServletRequest request, final ServletResponse response,
+            final FilterChain chain) throws ServletException, IOException {
+        // casting to HTTPServlet(Request|Response)
+        final HttpServletRequest req = (HttpServletRequest) request;
+        final HttpServletResponse res = (HttpServletResponse) response;
 
-		// X-frame-options (ClickJacking)
-		res.addHeader(X_FRAME_OPTIONS_HEADER, headerValue);
+        chain.doFilter(req, res);
 
-		chain.doFilter(req, res);
-	}
+        // X-frame-options (ClickJacking)
+        res.setHeader(X_FRAME_OPTIONS_HEADER, headerValue);
+    }
 }
