a6a347db509f69d5570acd7c1a8ebc90018d7add
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2021-08-18 16:13:21 +0200

    feat(permission): remove call to profile entry (#746)
    `GetUserRightsHelper` was retrieving permission of accessing a page
    using profile entries. We now only use the apps to do that.
    
    Remove the permission check on PageDownloadServlet, it is already handled by the Authorization filter.
    
    Relates to [RUNTIME-416](https://bonitasoft.atlassian.net/browse/RUNTIME-416)

--- server/src/main/java/org/bonitasoft/console/common/server/page/CustomPageAuthorizationsHelper.java
+++ server/src/main/java/org/bonitasoft/console/common/server/page/CustomPageAuthorizationsHelper.java
@@ -15,48 +15,30 @@ import org.bonitasoft.engine.session.APISession;
 
 import org.bonitasoft.livingapps.ApplicationModel;
 import org.bonitasoft.livingapps.ApplicationModelFactory;
-import org.bonitasoft.web.toolkit.client.common.util.StringUtil;
 
 /**
  * @author Julien Mege
  */
 public class CustomPageAuthorizationsHelper {
 
-    //All custom pages with this token suffix are authorized (should be sync with ApplicationFactoryClient)
-    public static final String BONITA_LABS_PAGE_TOKEN_EXTENSION = "BonitaLabs";
-    
-    private final GetUserRightsHelper getUserRightsHelper;
     private final ApplicationAPI applicationAPI;
     private final PageAPI pageApi;
-    private final APISession sessionApi;
+    private final APISession apiSession;
     private final ApplicationModelFactory applicationFactory;
 
-    public CustomPageAuthorizationsHelper(final GetUserRightsHelper getUserRightsHelper, final ApplicationAPI applicationAPI, final PageAPI pageApi, final ApplicationModelFactory applicationModelFactory) throws BonitaHomeNotSetException, ServerAPIException, UnknownAPITypeException {
-        this.getUserRightsHelper = getUserRightsHelper;
+    public CustomPageAuthorizationsHelper(final APISession apiSession, final ApplicationAPI applicationAPI, final PageAPI pageApi, final ApplicationModelFactory applicationModelFactory) throws BonitaHomeNotSetException, ServerAPIException, UnknownAPITypeException {
         this.applicationAPI = applicationAPI;
         this.pageApi = pageApi;
-        this.sessionApi = getUserRightsHelper.getApiSession();
+        this.apiSession = apiSession;
         this.applicationFactory = applicationModelFactory;
     }
 
-    public boolean isPageAuthorized(final String appToken, final String pageName) throws BonitaException {
-        if (StringUtil.isBlank(appToken)) {
-            return isPageAuthorizedInPortal(pageName);
-        } else {
-            return isPageAuthorizedInApplication(appToken, pageName);
-        }
-    }
-
-    private boolean isPageAuthorizedInPortal(final String pageName) throws BonitaException {
-        return getUserRightsHelper.getUserRights().contains(pageName) || pageName.endsWith(BONITA_LABS_PAGE_TOKEN_EXTENSION);
-    }
-
-    private boolean isPageAuthorizedInApplication(final String applicationToken, final String pageToken) throws BonitaException {
+    public boolean isPageAuthorized(final String applicationToken, final String pageToken) {
         try {
             Long applicationId = getApplicationId(applicationToken);
             final ApplicationModel application = applicationFactory.createApplicationModel(applicationToken);
 
-            if (applicationId == null || !application.authorize(sessionApi)) {
+            if (applicationId == null || !application.authorize(apiSession)) {
                 return false;
             }
 
@@ -73,7 +55,7 @@ public class CustomPageAuthorizationsHelper {
         SearchResult<Application> applicationSResult = applicationAPI.searchApplications(new SearchOptionsBuilder(0, 1)
                 .filter(ApplicationSearchDescriptor.TOKEN, applicationToken).done());
 
-        if (applicationSResult.getResult().size() < 1) {
+        if (applicationSResult.getResult().isEmpty()) {
             return null;
         }
 
