bad5260a220889aa89083910e56c2174a2e32a6e
abirembaut <anthony.birembaut@gmail.com>
2022-03-09 14:21:37 +0100

    fix(CSRF token): avoid token cookie being created with a wrong path (#857)
    when bonita webapp is deployed at root level CSRF token cookie is
    created with a wrong path (because the path is set to empty string which
    result to the browser setting the full info path as cookie path).
    * set the cookie path to / in this case.
    * add tests
    
    Covers [RUNTIME-921](https://bonitasoft.atlassian.net/browse/RUNTIME-921)

--- server/src/test/java/org/bonitasoft/console/common/server/login/PortalCookiesTest.java
+++ server/src/test/java/org/bonitasoft/console/common/server/login/PortalCookiesTest.java
@@ -1,31 +1,44 @@
 package org.bonitasoft.console.common.server.login;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.*;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.junit.Test;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class PortalCookiesTest {
-
-    private MockHttpServletRequest request = new MockHttpServletRequest();
     
-    private PortalCookies portalCookies = new PortalCookies();
+    @Mock
+    HttpServletRequest request;
+    
+    @Mock
+    HttpServletResponse response;
+    
+    private PortalCookies portalCookies = spy(new PortalCookies());
 
     @Test
     public void should_add_tenant_cookie_to_response() throws Exception {
-        MockHttpServletResponse response = new MockHttpServletResponse();
 
         portalCookies.addTenantCookieToResponse(response, 123L);
-
-        assertThat(response.getCookie("bonita.tenant").getValue()).isEqualTo("123");
+        
+        ArgumentCaptor<Cookie> argument = ArgumentCaptor.forClass(Cookie.class);
+        verify(response).addCookie(argument.capture());
+        assertThat(argument.getValue().getValue()).isEqualTo("123");
     }
 
     @Test
     public void should_get_tenant_cookie_from_request() throws Exception {
-        request.setCookies(new Cookie("bonita.tenant", "123"));
+        
+        doReturn(new Cookie[]{new Cookie("bonita.tenant", "123")}).when(request).getCookies();
 
         String tenant = portalCookies.getTenantCookieFromRequest(request);
 
@@ -34,11 +47,62 @@ public class PortalCookiesTest {
 
     @Test
     public void should_get_cookie_by_name_from_request() throws Exception {
+        
         Cookie cookie = new Cookie("bonita.tenant", "123");
-        request.setCookies(cookie);
+        doReturn(new Cookie[]{cookie}).when(request).getCookies();
 
         Cookie fetchedCookie = portalCookies.getCookie(request, "bonita.tenant");
 
         assertThat(fetchedCookie).isEqualTo(cookie);
+    }
+    
+    @Test
+    public void should_add_csrf_cookie_to_response_with_context_path() throws Exception {
+        
+        doReturn(true).when(portalCookies).isCSRFTokenCookieSecure();
+        doReturn("/bonita").when(request).getContextPath();
+
+        portalCookies.addCSRFTokenCookieToResponse(request, response, "apiTokenFromClient");
+
+        ArgumentCaptor<Cookie> argument = ArgumentCaptor.forClass(Cookie.class);
+        verify(response).addCookie(argument.capture());
+        assertThat(argument.getValue().getValue()).isEqualTo("apiTokenFromClient");
+        assertThat(argument.getValue().getPath()).isEqualTo("/bonita");
+        assertThat(argument.getValue().getSecure()).isTrue();
+    }
+    
+    @Test
+    public void should_add_csrf_cookie_to_response_with_empty_context_path() throws Exception {
+
+        doReturn(false).when(portalCookies).isCSRFTokenCookieSecure();
+        doReturn("").when(request).getContextPath();
+        
+        portalCookies.addCSRFTokenCookieToResponse(request, response, "apiTokenFromClient");
+
+        ArgumentCaptor<Cookie> argument = ArgumentCaptor.forClass(Cookie.class);
+        verify(response).addCookie(argument.capture());
+        assertThat(argument.getValue().getValue()).isEqualTo("apiTokenFromClient");
+        assertThat(argument.getValue().getPath()).isEqualTo("/");
+        assertThat(argument.getValue().getSecure()).isFalse();
+    }
+    
+    @Test
+    public void should_add_csrf_cookie_to_response_with_specified_path() throws Exception {
+
+        try {
+            System.setProperty("bonita.csrf.cookie.path", "/");
+            doReturn(false).when(portalCookies).isCSRFTokenCookieSecure();
+            doReturn("/bonita").when(request).getContextPath();
+            
+            portalCookies.addCSRFTokenCookieToResponse(request, response, "apiTokenFromClient");
+    
+            ArgumentCaptor<Cookie> argument = ArgumentCaptor.forClass(Cookie.class);
+            verify(response).addCookie(argument.capture());
+            assertThat(argument.getValue().getValue()).isEqualTo("apiTokenFromClient");
+            assertThat(argument.getValue().getPath()).isEqualTo("/");
+            assertThat(argument.getValue().getSecure()).isFalse();
+        } finally {
+            System.clearProperty("bonita.csrf.cookie.path");
+        }
     }
 }
