1d16cbb43315be91a5c0c54c1e7f60b7c6a0dd1f
abirembaut <anthony.birembaut@gmail.com>
2022-03-23 16:23:22 +0100

    fix(error pages): avoid loop on 404 errors without session (#864)
    - redirect to a fallback page when getting a 404 without any bonita session
    - redirect to a fallback page when getting a 500 because the platform is down
    - handle 403 as well in any of those 2 previous situations
    
    Covers [RUNTIME-971](https://bonitasoft.atlassian.net/browse/RUNTIME-971)
    
    Co-authored-by: VishalThapaliya <vishal.thapaliya@gmail.com>

--- server/src/test/java/org/bonitasoft/console/common/server/servlet/ErrorPageServletTest.java
+++ server/src/test/java/org/bonitasoft/console/common/server/servlet/ErrorPageServletTest.java
@@ -1,7 +1,10 @@
 package org.bonitasoft.console.common.server.servlet;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.File;
@@ -11,10 +14,13 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.nio.file.Paths;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.bonitasoft.engine.session.APISession;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -34,6 +40,15 @@ public class ErrorPageServletTest {
     @Mock
     ServletContext sc;
     
+    @Mock
+    RequestDispatcher requestDispatcher;
+
+    @Mock
+    HttpSession session;
+    
+    @Mock
+    APISession apiSession;
+
     @Spy
     ErrorPageServlet errorServlet = new ErrorPageServlet();
 
@@ -41,12 +56,16 @@ public class ErrorPageServletTest {
 
     @Before
     public void setUp() throws Exception {
+        when(request.getSession()).thenReturn(session);
+        when(session.getAttribute("apiSession")).thenReturn(apiSession);
+        doReturn(true).when(errorServlet).isPlatformHealthy();
         stringWriter = new StringWriter();
         PrintWriter printWriter = new PrintWriter(stringWriter);
         when(response.getWriter()).thenReturn(printWriter);
         File errorsHTMLFilePaths = Paths.get("src/main/webapp/WEB-INF/errors.html").toFile();
         InputStream errorPageInputStream = new FileInputStream(errorsHTMLFilePaths);
         when(sc.getResourceAsStream(ErrorPageServlet.ERROR_TEMPLATE_PATH)).thenReturn(errorPageInputStream);
+        when(sc.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
         doReturn(sc).when(errorServlet).getServletContext();
     }
     
@@ -83,5 +102,41 @@ public class ErrorPageServletTest {
         errorServlet.doGet(request, response);
         
         assertThat(stringWriter.toString()).contains("Status code missing from request.");
+    }
+    
+    @Test
+    public void should_forward_request_when_session_is_missing() throws Exception {
+        
+        when(request.getPathInfo()).thenReturn("/404");
+        when(request.getContextPath()).thenReturn("/bonita");
+        doReturn(false).when(errorServlet).isPlatformHealthy();
+
+        errorServlet.doGet(request, response);
+        
+        verify(sc, times(1)).getRequestDispatcher("/404.html");
+    }
+    
+    @Test
+    public void should_forward_request_when_session_is_missing_forbidden_access() throws Exception {
+        
+        when(request.getPathInfo()).thenReturn("/403");
+        when(request.getContextPath()).thenReturn("/bonita");
+        doReturn(false).when(errorServlet).isPlatformHealthy();
+
+        errorServlet.doGet(request, response);
+        
+        verify(sc, times(1)).getRequestDispatcher("/403.html");
+    }
+    
+    @Test
+    public void should_forward_request_when_platform_is_down() throws Exception {
+        
+        when(request.getPathInfo()).thenReturn("/500");
+        when(request.getContextPath()).thenReturn("/bonita");
+        when(session.getAttribute("apiSession")).thenReturn(null);
+
+        errorServlet.doGet(request, response);
+        
+        verify(sc, times(1)).getRequestDispatcher("/500.html");
     }
 }
